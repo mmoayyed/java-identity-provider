a30a005daaa4375079e60e9c9510d6be40dc015b
Will Norris <will@willnorris.com>
2007-01-22 20:33:39 +0000

    reworked the resolver implementation, moving validation into the resolver itself and cleaning up the resolution methods

--- src/edu/internet2/middleware/shibboleth/common/attribute/resolver/impl/AttributeResolverImpl.java
+++ src/edu/internet2/middleware/shibboleth/common/attribute/resolver/impl/AttributeResolverImpl.java
@@ -39,7 +39,6 @@ import edu.internet2.middleware.shibboleth.common.attribute.resolver.DataConnect
 import edu.internet2.middleware.shibboleth.common.attribute.resolver.PrincipalConnector;
 import edu.internet2.middleware.shibboleth.common.attribute.resolver.ResolutionContext;
 import edu.internet2.middleware.shibboleth.common.attribute.resolver.ResolutionPlugIn;
-import edu.internet2.middleware.shibboleth.common.attribute.resolver.ResolutionPlugInMap;
 
 /**
  * Primary implementation of {@link AttributeResolver}.
@@ -61,22 +60,13 @@ public class AttributeResolverImpl implements AttributeResolver {
     /** Prinicpal connectors defined for this resolver. */
     private Map<String, PrincipalConnector> principalConnectors;
 
-    /** Directed Graph of plug-in dependencies. */
-    private DirectedGraph<ResolutionPlugIn, DefaultEdge> dependencyGraph;
-
     /**
      * Constructor.
      */
     public AttributeResolverImpl() {
-        dependencyGraph = new SimpleDirectedGraph<ResolutionPlugIn, DefaultEdge>(DefaultEdge.class);
-        
-        // cycle detector shared by all plug-in maps
-        CycleDetector<ResolutionPlugIn, DefaultEdge> cycleDetector = new CycleDetector<ResolutionPlugIn, DefaultEdge>(
-                dependencyGraph);
-
-        dataConnectors = new ResolutionPlugInMap<DataConnector>(this, dependencyGraph, cycleDetector);
-        definitions = new ResolutionPlugInMap<AttributeDefinition>(this, dependencyGraph, cycleDetector);
-        principalConnectors = new ResolutionPlugInMap<PrincipalConnector>(this, dependencyGraph, cycleDetector);
+        dataConnectors = new FastMap<String, DataConnector>();
+        definitions = new FastMap<String, AttributeDefinition>();
+        principalConnectors = new FastMap<String, PrincipalConnector>();
     }
 
     /** {@inheritDoc} */
@@ -119,37 +109,41 @@ public class AttributeResolverImpl implements AttributeResolver {
      * 
      * @param id id of the attribute definition to resolve
      * @param context resolution context that we are working in
-     * @param resolvedAttributes attributes that have already been resolved during this request
+     * @return resolution of the specified attribute definition
      * @throws AttributeResolutionException if unable to resolve the requested attribute definition
      */
-    private void resolveAttribute(String id, ResolutionContext context, Map<String, Attribute> resolvedAttributes)
-            throws AttributeResolutionException {
-        AttributeDefinition currentDefinition = definitions.get(id);
+    private Attribute resolveAttribute(String id, ResolutionContext context) throws AttributeResolutionException {
+        AttributeDefinition definition = getAttributeDefinitions().get(id);
 
-        if (currentDefinition == null) {
-            return;
+        if (definition == null) {
+            return null;
         }
 
-        // Check to see if we have already resolved the attribute
-        if (resolvedAttributes.containsKey(id)) {
-            log.debug("Attribute (" + id + ") already resolved for this request.");
-            return;
+        // resolve attribute and all dependencies if not done so already
+        if (!context.getResolvedAttributeDefinitions().containsKey(id)) {
+
+            // wrap attribute definition for use within the given resolution context
+            AttributeDefinition contextualDefinition = new ContextualAttributeDefinition(definition);
+            definition = contextualDefinition;
+
+            // resolve DataConnector dependencies
+            for (String dependency : definition.getDataConnectorDependencyIds()) {
+                log.debug("Attribute (" + id + ") depends on connector (" + dependency + ").");
+                resolveConnector(dependency, context);
+            }
+
+            // resolve AttributeDefinition dependencies
+            for (String dependency : definition.getAttributeDefinitionDependencyIds()) {
+                log.debug("Attribute (" + id + ") depends on attribute (" + dependency + ").");
+                resolveAttribute(dependency, context);
+            }
+
+            // register definition as resolved for this resolution context
+            context.getResolvedAttributeDefinitions().put(id, definition);
         }
 
-        // resolve DataConnector dependencies
-        for (String dependency : currentDefinition.getDataConnectorDependencyIds()) {
-            log.debug("Attribute (" + id + ") depends on connector (" + dependency + ").");
-            resolveConnector(dependency, context, resolvedAttributes);
-        }
-
-        // resolve AttributeDefinition dependencies
-        for (String dependency : currentDefinition.getAttributeDefinitionDependencyIds()) {
-            log.debug("Attribute (" + id + ") depends on attribute (" + dependency + ").");
-            resolveAttribute(dependency, context, resolvedAttributes);
-        }
-
-        resolvedAttributes.put(id, currentDefinition.resolve(context));
-        context.getResolvedAttributeDefinitions().put(id, currentDefinition);
+        // return the actual resolution of the definition
+        return definition.resolve(context);
     }
 
     /** {@inheritDoc} */
@@ -160,20 +154,24 @@ public class AttributeResolverImpl implements AttributeResolver {
 
         // if no attributes requested, then resolve everything
         if (attributeIDs == null || attributeIDs.isEmpty()) {
-            attributeIDs = definitions.keySet();
+            attributeIDs = getAttributeDefinitions().keySet();
         }
 
         for (String id : attributeIDs) {
-            if (!definitions.containsKey(id)) {
+            if (getAttributeDefinitions().containsKey(id)) {
                 log.warn("No plug-in registered for attribute: (" + id + ")");
             } else {
                 log.info("Resolving attribute: (" + id + ")");
 
-                if (resolutionContext.getResolvedAttributeDefinitions().containsKey(id)) {
+                if (resolvedAttributes.containsKey(id)) {
                     log.debug("Attribute (" + id
                             + ") already resolved for this request.  No need for further resolution.");
                 } else {
-                    resolveAttribute(id, resolutionContext, resolvedAttributes);
+                    Attribute resolution = resolveAttribute(id, resolutionContext);
+
+                    if (resolution != null) {
+                        resolvedAttributes.put(resolution.getID(), resolution);
+                    }
                 }
             }
         }
@@ -186,48 +184,94 @@ public class AttributeResolverImpl implements AttributeResolver {
      * 
      * @param id id of the data connector to resolve
      * @param context resolution context that we are working in
-     * @param resolvedAttributes attributes that have already been resolved during this request
      * @throws AttributeResolutionException if unable to resolve the requested connector
      */
-    private void resolveConnector(String id, ResolutionContext context, Map<String, Attribute> resolvedAttributes)
-            throws AttributeResolutionException {
-        DataConnector dataConnector = dataConnectors.get(id);
+    private void resolveConnector(String id, ResolutionContext context) throws AttributeResolutionException {
+        DataConnector dataConnector = getDataConnectors().get(id);
 
         if (dataConnector == null) {
             return;
         }
 
-        // Check to see if we have already resolved the attribute
-        if (context.getResolvedDataConnectors().containsKey(id)) {
-            return;
+        // resolve connector and all dependencies if not done so already
+        if (!context.getResolvedDataConnectors().containsKey(id)) {
+            log.debug("Resolving DataConnector: (" + id + ").");
+
+            // wrap connector for use within the given resolution context
+            DataConnector contextualConnector = new ContextualDataConnector(dataConnector);
+            dataConnector = contextualConnector;
+
+            // resolve DataConnector dependencies
+            for (String dependency : dataConnector.getDataConnectorDependencyIds()) {
+                resolveConnector(dependency, context);
+            }
+
+            // resolve AttributeDefinition dependencies
+            for (String dependency : dataConnector.getAttributeDefinitionDependencyIds()) {
+                resolveAttribute(dependency, context);
+            }
+
+            try {
+                dataConnector.resolve(context);
+            } catch (AttributeResolutionException e) {
+                // TODO: use failover connector here?
+                return;
+            }
+
+            // register connector as resolved for this resolution context
+            context.getResolvedDataConnectors().put(id, dataConnector);
         }
-
-        // wrap connector for use within the given resolution context
-        DataConnector contextualConnector = new ContextualDataConnector(dataConnector);
-        dataConnector = contextualConnector;
-
-        // resolve DataConnector dependencies
-        for (String dependency : dataConnector.getDataConnectorDependencyIds()) {
-            resolveConnector(dependency, context, resolvedAttributes);
-        }
-
-        // resolve AttributeDefinition dependencies
-        for (String dependency : dataConnector.getAttributeDefinitionDependencyIds()) {
-            resolveAttribute(dependency, context, resolvedAttributes);
-        }
-
-        try {
-            dataConnector.resolve(context);
-        } catch (AttributeResolutionException e) {
-            // TODO: use failover connector
-        }
-
-        context.getResolvedDataConnectors().put(id, dataConnector);
     }
 
     /** {@inheritDoc} */
     public void setMetadataProvider(MetadataProvider provider) {
         this.metadataProvider = provider;
+    }
+
+    /** {@inheritDoc} */
+    public void validate() throws AttributeResolutionException {
+        DirectedGraph<ResolutionPlugIn, DefaultEdge> dependencyGraph;
+        CycleDetector<ResolutionPlugIn, DefaultEdge> cycleDetector;
+
+        dependencyGraph = new SimpleDirectedGraph<ResolutionPlugIn, DefaultEdge>(DefaultEdge.class);
+        cycleDetector = new CycleDetector<ResolutionPlugIn, DefaultEdge>(dependencyGraph);
+
+        for (AttributeDefinition definition : getAttributeDefinitions().values()) {
+            addVertex(dependencyGraph, definition);
+        }
+
+        for (DataConnector connector : getDataConnectors().values()) {
+            addVertex(dependencyGraph, connector);
+        }
+
+        // check for a dependency loop
+        if (cycleDetector.detectCycles()) {
+            throw new AttributeResolutionException("Attribute Resolver contains a resolution plug-in dependency loop.");
+        }
+    }
+
+    /**
+     * Add a resolution plug-in and dependencies to a directed graph.
+     * 
+     * @param graph directed graph.
+     * @param plugin plug-in to add.
+     */
+    private void addVertex(DirectedGraph<ResolutionPlugIn, DefaultEdge> graph, ResolutionPlugIn<?> plugin) {
+        graph.addVertex(plugin);
+
+        // add edges for attribute definition dependencies
+        for (String id : plugin.getAttributeDefinitionDependencyIds()) {
+            AttributeDefinition dependency = getAttributeDefinitions().get(id);
+            graph.addVertex(dependency);
+            graph.addEdge(plugin, dependency);
+        }
+
+        // add edges for data connector dependencies
+        for (String id : plugin.getDataConnectorDependencyIds()) {
+            DataConnector dependency = getDataConnectors().get(id);
+            graph.addVertex(dependency);
+            graph.addEdge(plugin, dependency);
+        }
     }
 
 }
