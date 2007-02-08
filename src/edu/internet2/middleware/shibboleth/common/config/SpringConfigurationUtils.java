530c4f295d4c7641ac3de374cc39606fcbfa12c9
Will Norris <will@willnorris.com>
2007-02-08 02:29:59 +0000

    utils class for spring configuration.

---
+++ src/edu/internet2/middleware/shibboleth/common/config/SpringConfigurationUtils.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright [2007] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.internet2.middleware.shibboleth.common.config;
+
+import org.apache.log4j.Logger;
+import org.opensaml.xml.util.XMLHelper;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParserDelegate;
+import org.springframework.beans.factory.xml.NamespaceHandler;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+/**
+ * Utilities to help configure Spring beans.
+ */
+public final class SpringConfigurationUtils {
+
+    /** Log4j logger. */
+    private static Logger log = Logger.getLogger(SpringConfigurationUtils.class);
+
+    /** Private Constructor. */
+    private SpringConfigurationUtils() {
+    }
+
+    /**
+     * xsi:type aware version of BeanDefinitionParserDelegate.parseCustomElement(Element).
+     * 
+     * @param element element to parse
+     * @param parserContext current parser context
+     * @return bean definition
+     */
+    public static BeanDefinition parseCustomElement(Element element, ParserContext parserContext) {
+        BeanDefinitionParserDelegate delegate = parserContext.getDelegate();
+        String namespaceUri = element.getNamespaceURI();
+
+        if (XMLHelper.hasXSIType(element)) {
+            namespaceUri = XMLHelper.getXSIType(element).getNamespaceURI();
+        }
+
+        NamespaceHandler handler = delegate.getReaderContext().getNamespaceHandlerResolver().resolve(namespaceUri);
+        if (handler == null) {
+            log.error("Unable to locate NamespaceHandler for namespace [" + namespaceUri + "]");
+            return null;
+        }
+        return handler.parse(element, new ParserContext(delegate.getReaderContext(), delegate, null));
+    }
+}
