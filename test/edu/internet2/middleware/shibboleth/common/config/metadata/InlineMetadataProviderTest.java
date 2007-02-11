63c20d3fef5ad3043cebab7142079bc086397682
Chad La Joie <clajoie@gmail.com>
2007-02-11 14:02:33 +0000

    Fix up library versions and classpath
    Use new parser pool

--- test/edu/internet2/middleware/shibboleth/common/config/metadata/InlineMetadataProviderTest.java
+++ test/edu/internet2/middleware/shibboleth/common/config/metadata/InlineMetadataProviderTest.java
@@ -15,14 +15,27 @@
  */
 package edu.internet2.middleware.shibboleth.common.config.metadata;
 
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import junit.framework.TestCase;
 
+import org.apache.log4j.Logger;
 import org.opensaml.DefaultBootstrap;
 import org.opensaml.saml2.metadata.EntitiesDescriptor;
 import org.opensaml.saml2.metadata.provider.MetadataProvider;
+import org.opensaml.xml.parse.ClasspathResolver;
+import org.opensaml.xml.parse.LoggingErrorHandler;
+import org.springframework.beans.factory.xml.DocumentLoader;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+
 
 /**
  * Test that the configuration code for inline metadata providers works correctly.
@@ -46,9 +59,12 @@ public class InlineMetadataProviderTest extends TestCase {
         
         String springConfigFile = "/data/edu/internet2/middleware/shibboleth/common/config/metadata/InlineMetadataProvider.xml";
         springCtx = new GenericApplicationContext();
-        XmlBeanDefinitionReader configReader = new XmlBeanDefinitionReader(InlineMetadataProviderTest.this.springCtx);
-        configReader.setValidationMode(XmlBeanDefinitionReader.VALIDATION_NONE);
-        configReader.setNamespaceAware(true);
+        
+        XmlBeanDefinitionReader configReader = new XmlBeanDefinitionReader(springCtx);
+        configReader.setDocumentLoader(new ValidatingDocumentLoader());
+        //configReader.setEntityResolver(new ClasspathEntityResolver());
+        //configReader.setValidationMode(XmlBeanDefinitionReader.VALIDATION_NONE);
+        //configReader.setNamespaceAware(true);
         configReader.loadBeanDefinitions(new ClassPathResource(springConfigFile));
     }
     
@@ -60,5 +76,26 @@ public class InlineMetadataProviderTest extends TestCase {
         MetadataProvider provider = (MetadataProvider) springCtx.getBean(providerId);
         assertNotNull(provider);
         assertEquals(((EntitiesDescriptor)provider.getMetadata()).getName(), expectedName);
+    }
+    
+    public class ValidatingDocumentLoader implements DocumentLoader{
+
+        Logger log = Logger.getLogger(ValidatingDocumentLoader.class);
+        
+        /** {@inheritDoc} */
+        public Document loadDocument(InputSource inputSource, EntityResolver entityResolver, ErrorHandler errorHandler, int validationMode, boolean namespaceAware) throws Exception {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setAttribute("http://java.sun.com/xml/jaxp/properties/schemaLanguage",
+    "http://www.w3.org/2001/XMLSchema");
+            factory.setCoalescing(true);
+            factory.setIgnoringComments(true);
+            factory.setNamespaceAware(true);
+            factory.setValidating(true);
+            
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setErrorHandler(new LoggingErrorHandler(log));
+            builder.setEntityResolver(new ClasspathResolver());
+            return builder.parse(inputSource);
+        }
     }
 }
