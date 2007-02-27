bbb3184d60c7102308a76b09e06100333e9f4887
Chad La Joie <clajoie@gmail.com>
2007-02-27 13:03:30 +0000

    Added many more relying party config options.

--- src/edu/internet2/middleware/shibboleth/common/relyingparty/SAMLRelyingPartyConfiguration.java
+++ src/edu/internet2/middleware/shibboleth/common/relyingparty/SAMLRelyingPartyConfiguration.java
@@ -16,6 +16,8 @@
 
 package edu.internet2.middleware.shibboleth.common.relyingparty;
 
+import org.opensaml.xml.security.credential.Credential;
+
 /**
  * SAML specific relying party configurations.
  */
@@ -34,4 +36,62 @@ public interface SAMLRelyingPartyConfiguration extends RelyingPartyConfiguration
      * @return whether providing attributes in a response an authentication request is preferred
      */
     public boolean isAttributePushPreferred();
+
+    /**
+     * Gets the credential that should be used to sign a message. Credential <strong>MUST</strong> include a private
+     * key.
+     * 
+     * @return credential that should be used to sign a message
+     */
+    public Credential getSigningCredential();
+
+    /**
+     * Gets the credential that should be used to decrypt a message. Credential <strong>MUST</strong> include a private
+     * key.
+     * 
+     * @return credential that should be used to decrypt a message
+     */
+    public Credential getDecryptionCredential();
+
+    /**
+     * Gets the URI for the default NameID format.
+     * 
+     * @return URI for the default NameID format
+     */
+    public String getDefaultNameIDFormat();
+
+    /**
+     * Gets the URI for the default authentication method.
+     * 
+     * @return URI for the default authentication method
+     */
+    public String getDefaultAuthenticationMethod();
+
+    /**
+     * Gets the default artifact type.
+     * 
+     * @return default artifact type
+     */
+    public int getDefaultArtifactType();
+
+    /**
+     * Gets whether assertions should be signed.
+     * 
+     * @return whether assertions should be signed
+     */
+    public boolean signAssertions();
+
+    /**
+     * Gets whether NameIDs should be encrypted.
+     * 
+     * @return whether NameIDs should be encrypted
+     */
+    public boolean encryptNameID();
+
+    /**
+     * Gets whether assertions should be encrypted.
+     * 
+     * @return whether assertions should be encrypted
+     */
+    public boolean encryptAssertion();
 }
