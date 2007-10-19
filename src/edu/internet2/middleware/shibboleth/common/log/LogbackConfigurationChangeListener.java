c95741aa24f8787226a0548ccae9658a80370da4
Chad La Joie <clajoie@gmail.com>
2007-10-19 08:25:30 +0000

    Initialize new logging system at IdP startup

---
+++ src/edu/internet2/middleware/shibboleth/common/log/LogbackConfigurationChangeListener.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright [2007] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.internet2.middleware.shibboleth.common.log;
+
+import org.opensaml.util.resource.Resource;
+import org.opensaml.util.resource.ResourceChangeListener;
+import org.opensaml.util.resource.ResourceException;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.status.ErrorStatus;
+import ch.qos.logback.core.status.InfoStatus;
+import ch.qos.logback.core.status.StatusManager;
+
+/**
+ * Callback that may be registered for a watch logback configuration file.
+ */
+public class LogbackConfigurationChangeListener implements ResourceChangeListener {
+
+    /** {@inheritDoc} */
+    public void onResourceCreate(Resource resource) {
+        configureLogback(resource);
+    }
+
+    /** {@inheritDoc} */
+    public void onResourceDelete(Resource resource) {
+        // do nothing
+    }
+
+    /** {@inheritDoc} */
+    public void onResourceUpdate(Resource resource) {
+        configureLogback(resource);
+    }
+
+    /**
+     * Configures logback using the given resource as the Joran configuration file.
+     * 
+     * @param configuration logback configuration file
+     */
+    protected void configureLogback(Resource configuration) {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusManager statusManager = loggerContext.getStatusManager();
+        statusManager.add(new InfoStatus("Loading logging configuration file: " + configuration.getLocation(), this));
+        try {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(loggerContext);
+            loggerContext.shutdownAndReset();
+            configurator.doConfigure(configuration.getInputStream());
+        } catch (JoranException e) {
+            statusManager.add(new ErrorStatus("Error loading logging configuration file: "
+                    + configuration.getLocation(), this, e));
+        } catch (ResourceException e) {
+            statusManager.add(new ErrorStatus("Error loading logging configuration file: "
+                    + configuration.getLocation(), this, e));
+        }
+    }
+}
---
+++ src/edu/internet2/middleware/shibboleth/common/log/LogbackLoggingService.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright [2007] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.internet2.middleware.shibboleth.common.log;
+
+import java.util.Timer;
+
+import org.opensaml.util.resource.FilesystemResource;
+import org.opensaml.util.resource.Resource;
+import org.opensaml.util.resource.ResourceChangeWatcher;
+import org.opensaml.util.resource.ResourceException;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.status.ErrorStatus;
+import ch.qos.logback.core.status.StatusManager;
+
+/**
+ * Simple logging service that watches for logback configuration file changes and reloads the file when a change occurs.
+ */
+public class LogbackLoggingService {
+
+    /**
+     * Constructor.
+     *
+     * @param taskTimer resource watchdog task timer
+     * @param loggingConfigurationFile logback configuration file
+     * @param pollingFrequency frequency the configuration file should be checked for changes
+     */
+    public LogbackLoggingService(Timer taskTimer, String loggingConfigurationFile, long pollingFrequency) {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusManager statusManager = loggerContext.getStatusManager();
+        
+        try{
+            Resource configResource = new FilesystemResource(loggingConfigurationFile);
+            LogbackConfigurationChangeListener configChangeListener = new LogbackConfigurationChangeListener();
+            
+            ResourceChangeWatcher resourceWatcher = new ResourceChangeWatcher(configResource, pollingFrequency, 5);
+            resourceWatcher.getResourceListeners().add(configChangeListener);
+            
+            taskTimer.schedule(resourceWatcher, 0, pollingFrequency);
+        }catch(ResourceException e){
+            statusManager.add(new ErrorStatus("Error loading logging configuration file: "
+                    + loggingConfigurationFile, this, e));
+        }
+    }
+}
