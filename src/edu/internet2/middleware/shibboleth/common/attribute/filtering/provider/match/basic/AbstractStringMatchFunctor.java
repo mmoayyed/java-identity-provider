c3ff1e6845f0f3bb2e897842436ca60592755cf5
Will Norris <will@willnorris.com>
2008-01-07 18:32:41 +0000

    prevent NPE when value is null, but matchString is not.

--- src/edu/internet2/middleware/shibboleth/common/attribute/filtering/provider/match/basic/AbstractStringMatchFunctor.java
+++ src/edu/internet2/middleware/shibboleth/common/attribute/filtering/provider/match/basic/AbstractStringMatchFunctor.java
@@ -16,8 +16,6 @@
 
 package edu.internet2.middleware.shibboleth.common.attribute.filtering.provider.match.basic;
 
-
-
 /**
  * Base class for match function that match one string value against a given string value.
  */
@@ -25,62 +23,62 @@ public abstract class AbstractStringMatchFunctor extends AbstractMatchFunctor {
 
     /** String to match for a positive evaluation. */
     private String matchString;
-    
+
     /** Whether the match evaluation is case sensitive. */
     private boolean caseSensitive;
-    
+
     /**
      * Gets the string to match for a positive evaluation.
      * 
      * @return string to match for a positive evaluation
      */
-    public String getMatchString(){
+    public String getMatchString() {
         return matchString;
     }
-    
+
     /**
      * Sets the string to match for a positive evaluation.
      * 
      * @param match string to match for a positive evaluation
      */
-    public void setMatchString(String match){
+    public void setMatchString(String match) {
         matchString = match;
     }
-    
+
     /**
      * Gets whether the match evaluation is case sensitive.
      * 
      * @return whether the match evaluation is case sensitive
      */
-    public boolean isCaseSensitive(){
+    public boolean isCaseSensitive() {
         return caseSensitive;
     }
-    
+
     /**
      * Sets whether the match evaluation is case sensitive.
      * 
      * @param isCaseSensitive whether the match evaluation is case sensitive
      */
-    public void setCaseSensitive(boolean isCaseSensitive){
+    public void setCaseSensitive(boolean isCaseSensitive) {
         caseSensitive = isCaseSensitive;
     }
 
     /**
-     * Matches the given value against the provided match string.  {@link Object#toString()} is used to 
-     * produce the string value to evaluate.
+     * Matches the given value against the provided match string. {@link Object#toString()} is used to produce the
+     * string value to evaluate.
      * 
      * @param value the value to evaluate
      * 
      * @return true if the value matches the given match string, false if not
      */
-    protected boolean isMatch(Object value){
-        if(value == null && matchString == null){
-            return true;
+    protected boolean isMatch(Object value) {
+        if (value == null) {
+            return matchString == null;
         }
-        
-        if(caseSensitive){
+
+        if (caseSensitive) {
             return value.toString().equals(matchString);
-        }else{
+        } else {
             return value.toString().equalsIgnoreCase(matchString);
         }
     }
