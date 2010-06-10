192e797b5696bf7e3932e8bd078cf31de395ca0f
Chad La Joie <clajoie@gmail.com>
2010-06-10 23:50:10 +0000

    Add in filtering APIs

--- shib-idp-attribute/src/main/java/edu/internet2/middleware/shibboleth/idp/attribute/filtering/AttributeFilterContext.java
+++ shib-idp-attribute/src/main/java/edu/internet2/middleware/shibboleth/idp/attribute/filtering/AttributeFilterContext.java
@@ -14,79 +14,60 @@
  * limitations under the License.
  */
 
-package edu.internet2.middleware.shibboleth.idp.attribute.resolver;
+package edu.internet2.middleware.shibboleth.idp.attribute.filtering;
 
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
-
-import javax.security.auth.Subject;
-
-import net.jcip.annotations.NotThreadSafe;
 
 import org.joda.time.DateTime;
 import org.joda.time.chrono.ISOChronology;
 import org.opensaml.messaging.context.Context;
 import org.opensaml.messaging.context.Subcontext;
 import org.opensaml.messaging.context.SubcontextContainer;
-import org.opensaml.util.Assert;
 import org.opensaml.util.collections.LazyMap;
-import org.opensaml.util.collections.LazySet;
 
 import edu.internet2.middleware.shibboleth.idp.attribute.Attribute;
 
-/** A context which carries and collects information through an attribute resolution. */
-@NotThreadSafe
-public final class AttributeResolutionContext implements Context, Subcontext {
+/** Context used to collect data as attributes are filtered. */
+public final class AttributeFilterContext implements Context, Subcontext {
 
     /** Context which acts as the owner or parent of this context. */
     private SubcontextContainer parentContext;
-    
+
     /** Unique identifier of the context. */
     private String id;
 
     /** Instant this context was created. */
     private DateTime creationInstant;
 
-    /** IDs of attributes that have been requested to be resolved. */
-    private Set<String> requestAttributes;
-
-    /** Subject to whom the resolved attributes belong. */
-    private Subject requestSubject;
-
     /** Attributes which have been resolved. */
     private Map<String, Attribute<?>> resolvedAttributes;
 
-    /** Constructor. Generates a random ID for the context and sets creation time to now. */
-    public AttributeResolutionContext(Subject subject) {
-        this(subject, null);
-    }
-    
+    /** Attributes which have been resolved. */
+    private Map<String, Attribute<?>> filteredAttributes;
+
     /**
      * Constructor.
-     *
+     * 
      * @param subject the subject for whom attributes will be resolved
      * @param additionalData additional data about the context in which the resolution is taking place
      */
-    public AttributeResolutionContext(Subject subject, SubcontextContainer parent){
+    public AttributeFilterContext(SubcontextContainer parent) {
         id = UUID.randomUUID().toString();
         creationInstant = new DateTime(ISOChronology.getInstanceUTC());
-        
-        Assert.isNotNull(subject, "Attribute resolution context subject may not be null");
-        requestSubject = subject;
-        
+
         parentContext = parent;
         parent.addSubcontext(this);
-        
-        requestAttributes = new LazySet<String>();
+
         resolvedAttributes = new LazyMap<String, Attribute<?>>();
+        filteredAttributes = new LazyMap<String, Attribute<?>>();
     }
 
     /** {@inheritDoc} */
     public SubcontextContainer getOwner() {
         return parentContext;
     }
-    
+
     /** {@inheritDoc} */
     public String getId() {
         return id;
@@ -98,29 +79,20 @@ public final class AttributeResolutionContext implements Context, Subcontext {
     }
 
     /**
-     * Gets the subject whose attributes are resolved.
-     * 
-     * @return subject whose attributes are resolved, never null
-     */
-    public Subject getRequestSubject() {
-        return requestSubject;
-    }
-
-    /**
-     * Gets the set of attributes requested to be resolved.
-     * 
-     * @return set of attributes requested to be resolved, never null
-     */
-    public Set<String> getRequestAttributes() {
-        return requestAttributes;
-    }
-
-    /**
      * Gets the attributes that have been resolved, indexed by attribute ID.
      * 
      * @return attributes that have been resolved, never null
      */
     public Map<String, Attribute<?>> getResolvedAttributes() {
         return resolvedAttributes;
+    }
+
+    /**
+     * Gets the attributes, indexed by ID, left after the filtering process has run.
+     * 
+     * @return attributes left after the filtering process has run, never null
+     */
+    public Map<String, Attribute<?>> getFilteredAttributes() {
+        return filteredAttributes;
     }
 }
--- shib-idp-attribute/src/main/java/edu/internet2/middleware/shibboleth/idp/attribute/resolver/AttributeResolver.java
+++ shib-idp-attribute/src/main/java/edu/internet2/middleware/shibboleth/idp/attribute/resolver/AttributeResolver.java
@@ -16,30 +16,18 @@
 
 package edu.internet2.middleware.shibboleth.idp.attribute.resolver;
 
-import java.util.Map;
-
-import edu.internet2.middleware.shibboleth.idp.attribute.Attribute;
-
-/**
- * The service that resolves the attributes for a particular subject.
- * 
- * @param <RequestContextType> the type of attribute request context used by the resolver.
- */
+/** A service that resolves the attributes for a particular subject. */
 public interface AttributeResolver {
 
     /**
-     * Gets all the attributes for a given subject. While an initial attribute producer is given this does not mean
-     * every returned attribute is from that producer. The producer may return information that can be used by data
-     * connectors to contact other producers and retrieve attributes from them.
+     * Gets all the attributes for a given subject.
      * 
-     * @param requestContext the attribute resolution context to use to resolve attributes
-     * 
-     * @return the attributes describing the subject
+     * @param requestContext the attribute resolution context that identifies the request subject and accumulates the
+     *            resolved attributes
      * 
      * @throws AttributeResolutionException thrown if there is a problem resolving the attributes for the subject
      */
-    public Map<String, Attribute<?>> resolveAttributes(AttributeResolutionContext requestContext)
-            throws AttributeResolutionException;
+    public void resolveAttributes(AttributeResolutionContext requestContext) throws AttributeResolutionException;
 
     /**
      * Check that the Attribute Resolver is in a valid state and ready to begin receiving resolution requests.
