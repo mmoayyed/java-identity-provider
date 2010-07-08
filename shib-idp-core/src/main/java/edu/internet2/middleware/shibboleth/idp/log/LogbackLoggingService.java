d6a47f909d177d234d026b90bd19a3ff42ff3eda
Chad La Joie <clajoie@gmail.com>
2010-07-08 18:21:42 +0000

    Make logging service compile against new service interface, still untested at this point and still more work to do

--- shib-idp-core/src/main/java/edu/internet2/middleware/shibboleth/idp/log/LogbackLoggingService.java
+++ shib-idp-core/src/main/java/edu/internet2/middleware/shibboleth/idp/log/LogbackLoggingService.java
@@ -17,12 +17,15 @@
 package edu.internet2.middleware.shibboleth.idp.log;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import org.joda.time.DateTime;
 import org.joda.time.chrono.ISOChronology;
 import org.opensaml.util.Assert;
+import org.opensaml.util.Closeables;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 
@@ -41,6 +44,9 @@ import edu.internet2.middleware.shibboleth.idp.service.ServiceException;
  */
 public class LogbackLoggingService extends AbstractService implements ReloadableService {
 
+    /** URL to the fallback logback configuration found in the IdP jar. */
+    private URL fallbackConfiguraiton;
+
     /** Logging configuration resource. */
     private Resource configurationResource;
 
@@ -53,8 +59,14 @@ public class LogbackLoggingService extends AbstractService implements Reloadable
     /** Watcher that monitors the configuration resource for this service for changes. */
     private ServiceConfigChangeWatcher resourceWatcher;
 
-    /** The last time time the service was reloaded, in milliseconds since the epoch in the UTC time zone. */
-    private long lastReloadInstant;
+    /** The last time time the service was reloaded, whether successful or not. */
+    private DateTime lastReloadInstant;
+
+    /** The last time the service was reloaded successfully. */
+    private DateTime lastSuccessfulReleaseIntant;
+
+    /** The cause of the last reload failure, if the last reload failed. */
+    private Throwable reloadFailureCause;
 
     /**
      * Constructor.
@@ -67,6 +79,8 @@ public class LogbackLoggingService extends AbstractService implements Reloadable
     public LogbackLoggingService(String id, Resource loggingConfiguration, Timer backgroundTaskTimer,
             long pollingFrequency) {
         super(id);
+
+        fallbackConfiguraiton = LogbackLoggingService.class.getResource("/logback.xml");
 
         if (pollingFrequency > 0) {
             Assert.isNotNull(backgroundTaskTimer, "Resource polling timer may not be null");
@@ -81,14 +95,35 @@ public class LogbackLoggingService extends AbstractService implements Reloadable
     }
 
     /** {@inheritDoc} */
-    public long getLastReloadInstant() {
+    public void validate() throws ServiceException {
+        if (!configurationResource.exists()) {
+            throw new ServiceException("Logging service configuration file " + configurationResource.getFilename()
+                    + " does not exist.");
+        }
+    }
+
+    /** {@inheritDoc} */
+    public DateTime getLastReloadAttemptInstant() {
         return lastReloadInstant;
     }
 
     /** {@inheritDoc} */
+    public DateTime getLastSuccessfulReloadInstant() {
+        return lastSuccessfulReleaseIntant;
+    }
+
+    /** {@inheritDoc} */
+    public Throwable getReloadFailureCause() {
+        return reloadFailureCause;
+    }
+
+    /** {@inheritDoc} */
     public void reload() {
-        loadLoggingConfiguration();
-        lastReloadInstant = new DateTime(ISOChronology.getInstanceUTC()).getMillis();
+        try {
+            loadLoggingConfiguration();
+        } catch (ServiceException e) {
+            // TODO
+        }
     }
 
     /** {@inheritDoc} */
@@ -104,35 +139,67 @@ public class LogbackLoggingService extends AbstractService implements Reloadable
             resourceWatcher = new ServiceConfigChangeWatcher();
             resourcePollingTimer.schedule(resourceWatcher, resourcePollingFrequency, resourcePollingFrequency);
         }
-        lastReloadInstant = new DateTime(ISOChronology.getInstanceUTC()).getMillis();
     }
 
-    /** Reads and loads in a new logging configuration. */
-    protected void loadLoggingConfiguration() {
+    /** 
+     * Reads and loads in a new logging configuration. 
+     * 
+     * @throws ServiceException thrown if there is a problem loading the logging configuration
+     */
+    protected void loadLoggingConfiguration() throws ServiceException {
+        lastReloadInstant = new DateTime(ISOChronology.getInstanceUTC());
+
         LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
         StatusManager statusManager = loggerContext.getStatusManager();
-        statusManager.add(new InfoStatus("Loading new logging configuration", this));
 
-        String configurationUrl = null;
+        InputStream ins = null;
         try {
-            configurationUrl = configurationResource.getURL().toExternalForm();
-        } catch (IOException e) {
-            statusManager.add(new ErrorStatus("Unable to determine configuration resource location", this, e));
-            return;
-        }
+            statusManager.add(new InfoStatus("Loading new logging configuration file: "
+                    + configurationResource.getFilename(), this));
+            ins = configurationResource.getInputStream();
+            loadLoggingConfiguration(statusManager, loggerContext, ins);
+            reloadFailureCause = null;
+            lastSuccessfulReleaseIntant = lastReloadInstant;
+        } catch (Exception e) {
+            Closeables.closeQuiety(ins);
+            statusManager.add(new ErrorStatus("Error loading logging configuration file: "
+                    + configurationResource.getFilename(), this, e));
+            reloadFailureCause = e.getCause();
 
+            try {
+                statusManager.add(new InfoStatus("Loading fallback logging configuration", this));
+                ins = fallbackConfiguraiton.openStream();
+                loadLoggingConfiguration(statusManager, loggerContext, ins);
+            } catch (IOException ioe) {
+                Closeables.closeQuiety(ins);
+                statusManager.add(new ErrorStatus("Error loading fallback logging configuration", this, e));
+                throw new ServiceException("Unable to load fallback logging configuration");
+            }
+        } finally {
+            Closeables.closeQuiety(ins);
+        }
+    }
+
+    /**
+     * Loads a logging configuration in to the active logger context. Error messages are printed out to the status
+     * manager.
+     * 
+     * @param statusManager status manager that will receive the error messages
+     * @param loggerContext active, to-be-configured, logger context
+     * @param loggingConfig logging configuration file
+     * 
+     * @throws ServiceException thrown is there is a problem loading the logging configuration
+     */
+    protected void loadLoggingConfiguration(StatusManager statusManager, LoggerContext loggerContext,
+            InputStream loggingConfig) throws ServiceException {
         try {
             loggerContext.reset();
             JoranConfigurator configurator = new JoranConfigurator();
             configurator.setContext(loggerContext);
-            configurator.doConfigure(configurationResource.getInputStream());
+            configurator.doConfigure(loggingConfig);
             loggerContext.start();
         } catch (JoranException e) {
-            statusManager
-                    .add(new ErrorStatus("Error loading logging configuration file: " + configurationUrl, this, e));
-        } catch (IOException e) {
-            statusManager
-                    .add(new ErrorStatus("Error loading logging configuration file: " + configurationUrl, this, e));
+            throw new ServiceException(e);
         }
     }
 
@@ -141,25 +208,6 @@ public class LogbackLoggingService extends AbstractService implements Reloadable
 
         /** {@inheritDoc} */
         public void run() {
-            try {
-                if (lastReloadInstant == configurationResource.lastModified()) {
-                    return;
-                }
-            } catch (IOException e) {
-                LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-                StatusManager statusManager = loggerContext.getStatusManager();
-
-                String configurationUrl = null;
-                try {
-                    configurationUrl = configurationResource.getURL().toExternalForm();
-                } catch (IOException e2) {
-                    statusManager.add(new ErrorStatus("Unable to determine configuration resource location", this, e2));
-                    return;
-                }
-
-                statusManager.add(new InfoStatus("Loading logging configuration file: " + configurationUrl, this));
-            }
-
             reload();
         }
     }
