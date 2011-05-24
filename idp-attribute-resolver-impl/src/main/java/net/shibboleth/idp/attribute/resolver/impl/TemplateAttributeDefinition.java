5412b3d11577fe35463f7fca85809ae36902278d
Rod Widdowson <rdw@steadingsoftware.com>
2011-05-24 14:17:31 +0000

    IDP-17 - Add Template resolver definition and test.
    https://issues.shibboleth.net/jira/browse/IDP-17

---
+++ idp-attribute-resolver-impl/src/main/java/net/shibboleth/idp/attribute/resolver/impl/TemplateAttributeDefinition.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2011 University Corporation for Advanced Internet Development, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.attribute.resolver.impl;
+
+import java.io.StringWriter;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import net.jcip.annotations.NotThreadSafe;
+import net.shibboleth.idp.attribute.Attribute;
+import net.shibboleth.idp.attribute.resolver.AttributeResolutionContext;
+import net.shibboleth.idp.attribute.resolver.AttributeResolutionException;
+import net.shibboleth.idp.attribute.resolver.BaseAttributeDefinition;
+import net.shibboleth.idp.attribute.resolver.ResolverPluginDependency;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.resource.loader.StringResourceLoader;
+import org.apache.velocity.runtime.resource.util.StringResourceRepository;
+import org.opensaml.util.collections.LazyMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An attribute definition that constructs its values based on the values of its dependencies using the Velocity
+ * Template Language. Dependencies may have multiple values, however multiples dependencies must have the same number of
+ * values. In the case of multi-valued dependencies, the template will be evaluated multiples times, iterating over each
+ * dependency.
+ */
+@NotThreadSafe
+public class TemplateAttributeDefinition extends BaseAttributeDefinition {
+
+    /** Class logger. */
+    private static Logger log = LoggerFactory.getLogger(TemplateAttributeDefinition.class);
+
+    /** Velocity engine to use to render attribute values. */
+    private final VelocityEngine velocity;
+
+    /** Name the attribute template is registered under within the template engine. */
+    private final String templateName;
+
+    /** Template that produces the attribute value. */
+    private final String attributeTemplate;
+
+    /**
+     * IDs of the attributes used in this composite.
+     * 
+     * This is checked against dependencies
+     */
+    private final Set<String> sourceAttributes;
+
+    /**
+     * Constructor.
+     * 
+     * @param id the identifier for this definition.
+     * @param engine velocity engine used to parse template.
+     * @param template the template we use.
+     * @param attributes the attributes.
+     */
+    public TemplateAttributeDefinition(final String id, final VelocityEngine engine, final String template,
+            final List<String> attributes) {
+        super(id);
+        velocity = engine;
+        templateName = "shibboleth.resolver.ad." + id;
+        sourceAttributes = new HashSet<String>(attributes);
+        attributeTemplate = template;
+
+        //
+        // Register the template
+        //
+        StringResourceRepository repository = StringResourceLoader.getRepository();
+        repository.putStringResource(templateName, attributeTemplate.trim());
+    }
+
+    /**
+     * Set up a map which can be used to populate the template. The key is the attribute name and the value is the
+     * iterator to give all the names. We also return how deep the iteration will be and throw an exception if there is
+     * a mismatch.
+     * 
+     * Finally the names of the source attributes is checked against the dependency attributes and if there is a
+     * mismatch then a warning is emitted.
+     * 
+     * @param resolutionContext to look for dependencies in.
+     * @param sourceValues to populate with the attribute iterators
+     * @return how many values in the attributes
+     * @throws AttributeResolutionException if there is a mismatched count of attributes
+     */
+    private int countAndSetupSourceValues(final AttributeResolutionContext resolutionContext,
+            Map<String, Iterator> sourceValues) throws AttributeResolutionException {
+        int valueCount = -1;
+        Set<ResolverPluginDependency> depends = getDependencies();
+        Set<String> unresolvedAttributes = new HashSet<String>(sourceAttributes);
+
+        for (ResolverPluginDependency dep : depends) {
+            Attribute<?> dependentAttribute = dep.getDependentAttribute(resolutionContext);
+            Collection<?> values;
+            if (null == dependentAttribute) {
+                log.warn("Dependency of TemplateAttribute " + getId() + " returned null dependent attribute");
+                continue;
+            }
+            if (!sourceAttributes.contains(dependentAttribute.getId())) {
+                log.info("Dependent atribute " + dependentAttribute.getId()
+                        + " not a source attribute for template attribute definition " + getId());
+                continue;
+            }
+            //
+            // Take from the input list
+            //
+            unresolvedAttributes.remove(dependentAttribute.getId());
+
+            //
+            // And add the values to the output list
+            //
+            values = dependentAttribute.getValues();
+            if (null == values) {
+                log.warn("Dependency " + dependentAttribute.getId() + " of TemplateAttribute " + getId()
+                        + "returned null value set");
+                continue;
+            }
+            if (valueCount == -1) {
+                valueCount = values.size();
+            } else if (valueCount != values.size()) {
+                log.error("All attributes used in TemplateAttributeDefinition " + getId()
+                        + " must have the same number of values.");
+                throw new AttributeResolutionException("All attributes used in TemplateAttributeDefinition " + getId()
+                        + " must have the same number of values.");
+            }
+
+            sourceValues.put(dependentAttribute.getId(), values.iterator());
+        }
+        if (!unresolvedAttributes.isEmpty()) {
+            log.warn("Dependant attributes {} not used by TemplateAttributeDefinition {}",
+                    unresolvedAttributes.toArray(), getId());
+        }
+        return valueCount;
+    }
+
+    /** {@inheritDoc} */
+
+    protected Attribute<?> doAttributeResolution(final AttributeResolutionContext resolutionContext)
+            throws AttributeResolutionException {
+
+        Map<String, Iterator> sourceValues = new LazyMap<String, Iterator>();
+        Attribute<String> result = new Attribute<String>(getId());
+        int valueCount = countAndSetupSourceValues(resolutionContext, sourceValues);
+
+        if (null == getDependencies()) {
+            log.info("TemplateAttribute definition " + getId() + " had no dependencies");
+            return null;
+        }
+
+        // build velocity context
+        VelocityContext vCtx = new VelocityContext();
+        vCtx.put("requestContext", resolutionContext);
+        for (int i = 0; i < valueCount; i++) {
+            // Setup the attributes for this time around
+            for (String attributeId : sourceValues.keySet()) {
+                vCtx.put(attributeId, sourceValues.get(attributeId).next());
+            }
+
+            try {
+                log.debug("Populating the following {} template", templateName);
+
+                StringWriter output = new StringWriter();
+                Template template;
+                template = velocity.getTemplate(templateName);
+                template.merge(vCtx, output);
+                result.addValue(output.toString());
+
+            } catch (Exception e) {
+                //
+                // Yup, Velocity throws Exception....
+                //
+                log.error("Unable to populate " + templateName + " template", e);
+                throw new AttributeResolutionException("Unable to evaluate template", e);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ idp-attribute-resolver-impl/src/test/java/net/shibboleth/idp/attribute/resolver/impl/TemplateAttributeTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2011 University Corporation for Advanced Internet Development, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.attribute.resolver.impl;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import net.jcip.annotations.ThreadSafe;
+import net.shibboleth.idp.attribute.Attribute;
+import net.shibboleth.idp.attribute.resolver.AttributeResolutionContext;
+import net.shibboleth.idp.attribute.resolver.AttributeResolutionException;
+import net.shibboleth.idp.attribute.resolver.AttributeResolver;
+import net.shibboleth.idp.attribute.resolver.BaseAttributeDefinition;
+import net.shibboleth.idp.attribute.resolver.BaseDataConnector;
+import net.shibboleth.idp.attribute.resolver.ResolverPluginDependency;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.opensaml.util.collections.LazySet;
+import org.opensaml.xml.util.LazyList;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/** test for {@link net.shibboleth.idp.attribute.resolver.impl.TemplateAttribute}. */
+@ThreadSafe
+public class TemplateAttributeTest {
+
+    /** The name. */
+    private static final String TEST_ATTRIBUTE_NAME = "TEMPLATE";
+
+    /** Simple result. */
+    private static final String SIMPLE_VALUE = "simple";
+
+    /** A simple script to set a constant value. */
+    private static final String TEST_SIMPLE_TEMPLATE = SIMPLE_VALUE;
+
+    /** A simple script to set a value based on input values. */
+    private static final String TEST_ATTRIBUTES_TEMPLATE = "Att " + "${" + TestSources.DEPENDS_ON_ATTRIBUTE_NAME + "}-"
+            + "${" + TestSources.DEPENDS_ON_SECOND_ATTRIBUTE_NAME + "}";
+
+    /** Our singleton engine. */
+    private static VelocityEngine engineSingleton;
+
+    /**
+     * Create new or return the velocity engine with enough hardwired properties to get us going.
+     * 
+     * @return a new engine suitable groomed
+     */
+    private VelocityEngine getEngine() {
+        if (null == engineSingleton) {
+            engineSingleton = new VelocityEngine();
+            try {
+                engineSingleton.addProperty("string.resource.loader.class",
+                        "org.apache.velocity.runtime.resource.loader.StringResourceLoader");
+                engineSingleton.addProperty("classpath.resource.loader.class",
+                        "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+                engineSingleton.addProperty("resource.loader", "classpath, string");
+                engineSingleton.init();
+            } catch (Exception e) {
+                Assert.fail("couldn't create engine", e);
+            }
+        }
+        return engineSingleton;
+    }
+
+    /**
+     * Test resolution of an template script (statically generated data).
+     * 
+     * @throws AttributeResolutionException id resolution fails
+     */
+    @Test
+    public void testSimple() throws AttributeResolutionException {
+
+        TemplateAttributeDefinition attr =
+                new TemplateAttributeDefinition(TEST_ATTRIBUTE_NAME, getEngine(), TEST_ATTRIBUTES_TEMPLATE,
+                        new LazyList<String>());
+
+        Attribute<?> val = attr.doAttributeResolution(new AttributeResolutionContext(null));
+        Collection<?> results = val.getValues();
+
+        Assert.assertEquals(results.size(), 0, "Templated value count");
+    }
+
+    /**
+     * Test resolution of an template script (statically generated data). By giving it attributes we create some values.
+     * 
+     * @throws AttributeResolutionException if resolution fails
+     */
+    @Test
+    public void testSimpleWithValues() throws AttributeResolutionException {
+
+        List<String> sources = new LazyList<String>();
+        sources.add(TestSources.DEPENDS_ON_ATTRIBUTE_NAME);
+
+        TemplateAttributeDefinition templateDef =
+                new TemplateAttributeDefinition(TEST_ATTRIBUTE_NAME, getEngine(), TEST_SIMPLE_TEMPLATE, sources);
+        Set<ResolverPluginDependency> ds = new LazySet<ResolverPluginDependency>();
+        ds.add(new ResolverPluginDependency(TestSources.STATIC_ATTRIBUTE_NAME, TestSources.DEPENDS_ON_ATTRIBUTE_NAME));
+        templateDef.setDependencies(ds);
+
+        AttributeResolver resolver = new AttributeResolver("foo");
+
+        Set<BaseAttributeDefinition> attrDefinitions = new LazySet<BaseAttributeDefinition>();
+        attrDefinitions.add(templateDef);
+        attrDefinitions.add(TestSources.populatedStaticAttribute());
+        Set<BaseDataConnector> dataDefinitions = new LazySet<BaseDataConnector>();
+        dataDefinitions.add(TestSources.populatedStaticConnectior());
+        resolver.setDataConnectors(dataDefinitions);
+        resolver.setAttributeDefinition(attrDefinitions);
+
+        AttributeResolutionContext context = new AttributeResolutionContext(null);
+        resolver.resolveAttributes(context);
+
+        Attribute<?> a = context.getResolvedAttributes().get(TEST_ATTRIBUTE_NAME);
+        Collection results = a.getValues();
+        Assert.assertEquals(results.size(), 1, "Templated value count");
+        Assert.assertTrue(results.contains(SIMPLE_VALUE), "Single value context is correct");
+
+    }
+
+    /**
+     * Test resolution of an template script with data generated from the attributes.
+     * 
+     * @throws AttributeResolutionException if it goes wrong.
+     */
+    @Test
+    public void testTemplateWithValues() throws AttributeResolutionException {
+
+        List<String> sources = new LazyList<String>();
+        sources.add(TestSources.DEPENDS_ON_ATTRIBUTE_NAME);
+        sources.add(TestSources.DEPENDS_ON_SECOND_ATTRIBUTE_NAME);
+
+        TemplateAttributeDefinition templateDef =
+                new TemplateAttributeDefinition(TEST_ATTRIBUTE_NAME, getEngine(), TEST_ATTRIBUTES_TEMPLATE, sources);
+        Set<ResolverPluginDependency> ds = new LazySet<ResolverPluginDependency>();
+        ds.add(new ResolverPluginDependency(TestSources.STATIC_ATTRIBUTE_NAME, TestSources.DEPENDS_ON_ATTRIBUTE_NAME));
+        ds.add(new ResolverPluginDependency(TestSources.STATIC_CONNECTOR_NAME,
+                TestSources.DEPENDS_ON_SECOND_ATTRIBUTE_NAME));
+        templateDef.setDependencies(ds);
+
+        AttributeResolver resolver = new AttributeResolver("foo");
+
+        Set<BaseAttributeDefinition> attrDefinitions = new LazySet<BaseAttributeDefinition>();
+        attrDefinitions.add(templateDef);
+        attrDefinitions.add(TestSources.populatedStaticAttribute());
+        Set<BaseDataConnector> dataDefinitions = new LazySet<BaseDataConnector>();
+        dataDefinitions.add(TestSources.populatedStaticConnectior());
+        resolver.setDataConnectors(dataDefinitions);
+        resolver.setAttributeDefinition(attrDefinitions);
+
+        AttributeResolutionContext context = new AttributeResolutionContext(null);
+        resolver.resolveAttributes(context);
+
+        Attribute<?> a = context.getResolvedAttributes().get(TEST_ATTRIBUTE_NAME);
+        Collection results = a.getValues();
+        Assert.assertEquals(results.size(), 2, "Templated value count");
+        String s = "Att " + TestSources.COMMON_ATTRIBUTE_VALUE + "-" + TestSources.SECOND_ATTRIBUTE_VALUES[0];
+        Assert.assertTrue(results.contains(s), "First Match");
+        s = "Att " + TestSources.ATTRIBUTE_ATTRIBUTE_VALUE + "-" + TestSources.SECOND_ATTRIBUTE_VALUES[1];
+        Assert.assertTrue(results.contains(s), "Second Match");
+    }
+
+}
