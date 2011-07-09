7d4c331e047b16247e0e7d713d084dbf61c44287
Rod Widdowson <rdw@steadingsoftware.com>
2011-07-09 10:48:42 +0000

    IDP-37 - implement regexp value policy rule, base class and test
    https://issues.shibboleth.net/jira/browse/IDP-37

---
+++ idp-attribute-filter-impl/src/main/java/net/shibboleth/idp/attribute/filtering/impl/policy/AttributeValueRegexCriterion.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.attribute.filtering.impl.policy;
+
+import java.util.Collection;
+
+import net.shibboleth.idp.attribute.Attribute;
+import net.shibboleth.idp.attribute.filtering.AttributeFilterContext;
+
+import org.opensaml.util.Assert;
+import org.opensaml.util.StringSupport;
+import org.opensaml.xml.security.EvaluableCriteria;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implement the AttributeValueRegex policy activation criterion.
+ * 
+ * If the supplied attribute name has any value which matches the provided regex then we return true.
+ */
+public class AttributeValueRegexCriterion extends BaseRegexCriterion implements
+        EvaluableCriteria<AttributeFilterContext> {
+
+    /** Class logger. */
+    private final Logger log = LoggerFactory.getLogger(AttributeValueRegexCriterion.class);
+
+    /** The name of the target attribute. */
+    private final String attributeName;
+
+    /**
+     * Constructor.
+     * 
+     * @param regex the regexp we are matching with.
+     * @param attribute the name of the attribute
+     */
+    public AttributeValueRegexCriterion(final String regex, final String attribute) {
+        super(regex);
+        attributeName = StringSupport.trimOrNull(attribute);
+        Assert.isNotNull(attributeName, "AttributeValue Policy Requirement rule must have non null attribute name");
+    }
+
+    /** Private Constructor. Here uniquely to guarantee that we always have non null members. */
+    private AttributeValueRegexCriterion() {
+        super(null);
+        Assert.isTrue(false, "Private constructor should not be called");
+        attributeName = null;
+    }
+
+    /**
+     * Gets the name of the attribute under consideration.
+     * 
+     * @return the name of the attribute under consideration, never null or empty.
+     */
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    /** {@inheritDoc} */
+    public Boolean evaluate(final AttributeFilterContext target) {
+        Attribute<?> attribute = target.getPrefilteredAttributes().get(attributeName);
+
+        if (null == attribute) {
+            log.warn("PolicyRequirementRule: Could not locate atribute named " + attributeName);
+            return false;
+        }
+
+        Collection<?> values = attribute.getValues();
+        for (Object value : values) {
+            if (isMatch(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ idp-attribute-filter-impl/src/test/java/net/shibboleth/idp/attribute/filtering/impl/policy/TestAttributeValueRegex.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.attribute.filtering.impl.policy;
+
+import java.util.Set;
+
+import net.shibboleth.idp.attribute.Attribute;
+import net.shibboleth.idp.attribute.filtering.AttributeFilterContext;
+
+import org.opensaml.util.collections.CollectionSupport;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/** tests for the Attribute Value Regex criterion. */
+
+public class TestAttributeValueRegex {
+    
+    /** name used throughout the tests for the attribute. */
+    private static final String ATTR_NAME = "attributeName";
+
+    /** Test various combinations of bad parameters. */
+    @Test
+    public void attributeValueRegexCriterionBadParamsTest() {
+        try {
+            new AttributeValueRegexCriterion("", ATTR_NAME);
+            Assert.assertTrue(false, "testing bad constructor (empty match): unreacahble code");
+        } catch (IllegalArgumentException e) {
+            Assert.assertTrue(true, "testing bad constructor (empty match): usual case");
+        }
+
+        try {
+            new AttributeValueRegexCriterion("r.x", null);
+            Assert.assertTrue(false, "testing bad constructor (null attribute name): unreacahble code");
+        } catch (IllegalArgumentException e) {
+            Assert.assertTrue(true, "testing bad constructor (null attribute name): usual case");
+        }
+        //
+        // mismatched attribute
+        //
+        AttributeFilterContext filterContext = new AttributeFilterContext(null);
+        AttributeValueRegexCriterion filter = new AttributeValueRegexCriterion("r.x", ATTR_NAME);
+
+        Assert.assertFalse(filter.evaluate(filterContext), "missed attribute should return false");
+
+    }
+
+    /** test usual operation. */
+    @Test
+    public void attributeValueCriterionStringTest() {
+        Attribute<String> attribute = new Attribute<String>(ATTR_NAME);
+
+        attribute.setValues(CollectionSupport.toSet("one", "two", "three"));
+        AttributeFilterContext filterContext = new AttributeFilterContext(null);
+
+        Set s = CollectionSupport.toSet(attribute);
+        filterContext.setPrefilteredAttributes(s);
+
+        AttributeValueRegexCriterion filter = new AttributeValueRegexCriterion("t.e", ATTR_NAME);
+        Assert.assertFalse(filter.evaluate(filterContext), "match for the string \"t.e\"");
+
+        filter = new AttributeValueRegexCriterion("t.*e", ATTR_NAME);
+        Assert.assertTrue(filter.evaluate(filterContext), "match for the string \"t.*e\" (against 'three')");
+
+    }
+
+}
