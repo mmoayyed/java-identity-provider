e795b722e59908f538a693523f797cd8b361cdd7
Rod Widdowson <rdw@steadingsoftware.com>
2013-03-06 13:13:20 +0000

    IDP-272  Port CryptoTransientIdAttributeDefinition to V3 APIs.  Test coverage still pending.

---
+++ idp-attribute-resolver-impl/src/main/java/net/shibboleth/idp/attribute/resolver/impl/ad/CryptoTransientIdAttributeDefinition.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.attribute.resolver.impl.ad;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.shibboleth.idp.attribute.Attribute;
+import net.shibboleth.idp.attribute.AttributeValue;
+import net.shibboleth.idp.attribute.StringAttributeValue;
+import net.shibboleth.idp.attribute.resolver.AttributeRecipientContext;
+import net.shibboleth.idp.attribute.resolver.AttributeResolutionContext;
+import net.shibboleth.idp.attribute.resolver.BaseAttributeDefinition;
+import net.shibboleth.idp.attribute.resolver.ResolutionException;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+
+/**
+ * An attribute definition that generates integrity protected, encrypted identifiers useful for stateless transient
+ * subject IDs.
+ */
+public class CryptoTransientIdAttributeDefinition extends BaseAttributeDefinition {
+
+    /** Class logger. */
+    private Logger log = LoggerFactory.getLogger(CryptoTransientIdAttributeDefinition.class);
+
+    /** Object used to protect and encrypt the data. */
+    private DataSealer dataSealer;
+
+    /** Length, in milliseconds, tokens are valid. */
+    private long idLifetime;
+
+    /** {@inheritDoc} */
+    protected void doInitialize() throws ComponentInitializationException {
+        if (0 == idLifetime) {
+            log.debug("Attribute definition {}: set default lifetime of 4 hours.", getId());
+            idLifetime = 1000 * 60 * 60 * 4;
+        }
+        if (null == dataSealer) {
+            throw new ComponentInitializationException("Attribute Resolver " + getId()
+                    + ": data sealer was null or unspecified");
+        }
+
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull protected Optional<Attribute> doAttributeDefinitionResolve(
+            @Nonnull AttributeResolutionContext resolutionContext) throws ResolutionException {
+
+        final AttributeRecipientContext attributeRecipientContext =
+                resolutionContext.getSubcontext(AttributeRecipientContext.class);
+
+        if (null == attributeRecipientContext) {
+            throw new ResolutionException("Attribute definition '" + getId()
+                    + ": no attribute recipient context provided ");
+        }
+
+        final String attributeIssuerID = attributeRecipientContext.getAttributeIssuerID();
+        if (null == attributeIssuerID) {
+            throw new ResolutionException("Attribute definition '" + getId()
+                    + ": provided attribute issuer ID was empty");
+        }
+
+        final String attributeRecipientID = attributeRecipientContext.getAttributeRecipientID();
+        if (null == attributeRecipientID) {
+            throw new ResolutionException("Attribute definition '" + getId()
+                    + ": provided attribute recipient ID was empty");
+        }
+
+        final String principalName = attributeRecipientContext.getPrincipal();
+        if (null == principalName) {
+            throw new ResolutionException("Attribute definition '" + getId() + ": provided prinicipal name was empty");
+        }
+
+        final Attribute result = new Attribute(getId());
+
+        log.debug("Attribute definition '{}': Building crypto transient ID for recipient: '{}',"
+                + "issuer: {}, principal identifer: {}", new Object[] {getId(), attributeRecipientID,
+                attributeIssuerID, principalName,});
+
+        StringBuilder principalTokenIdBuilder = new StringBuilder();
+        principalTokenIdBuilder.append(attributeIssuerID).append("!").append(attributeRecipientID).append("!")
+                .append(principalName);
+        
+        try {
+            String transientId =
+                    dataSealer.wrap(principalTokenIdBuilder.toString(), System.currentTimeMillis() + idLifetime);
+            Set<AttributeValue> vals = Collections.singleton((AttributeValue) new StringAttributeValue(transientId));
+            result.setValues(vals);
+        } catch (DataSealerException e) {
+            log.error("Attribute definition '" + getId() + "': Caught exception wrapping principal identifier.", e);
+        }
+        
+        return Optional.of(result);
+    }
+
+    /**
+     * Gets the time, in milliseconds, that ids are valid for.
+     * 
+     * @return time, in milliseconds, that ids are valid for.
+     */
+    public long getIdLifetime() {
+        return idLifetime;
+    }
+
+    /**
+     * Sets the time, in milliseconds, that ids are valid for.
+     * 
+     * @param lifetime time, in milliseconds, that ids are valid for.
+     */
+    public void setIdLifetime(long lifetime) {
+        idLifetime = lifetime;
+    }
+
+    /**
+     * Sets the Sealer.
+     * 
+     * @param sealer object used to protect and encrypt the data
+     */
+    public void setDataSealer(@Nonnull DataSealer sealer) {
+        dataSealer = Constraint.isNotNull(sealer, "DataSealer may not be null");
+    }
+
+    /**
+     * Gets the Sealer.
+     * 
+     * @return sealer object used to protect and encrypt the data
+     */
+    @Nullable public DataSealer getDataSealer() {
+        return dataSealer;
+    }
+
+}
---
+++ idp-attribute-resolver-impl/src/test/java/net/shibboleth/idp/attribute/resolver/impl/ad/CryptoTransientIdAttributeDefinitionTest.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.attribute.resolver.impl.ad;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+
+import net.shibboleth.idp.attribute.Attribute;
+import net.shibboleth.idp.attribute.AttributeValue;
+import net.shibboleth.idp.attribute.StringAttributeValue;
+import net.shibboleth.idp.attribute.resolver.AttributeResolutionContext;
+import net.shibboleth.idp.attribute.resolver.ResolutionException;
+import net.shibboleth.idp.attribute.resolver.impl.TestSources;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.google.common.base.Optional;
+
+/**
+ * Tests for {Link CryptoTransientIdAttributeDefinition}.
+ */
+
+public class CryptoTransientIdAttributeDefinitionTest {
+
+    private static final String ID = "CryptoTransientIdAttributeDefn";
+
+    private static final long TIMEOUT = 500;
+
+    private DataSealer dataSealer;
+
+    /**
+     * Copy the JKS from the classpath into the filesystem and get the name.
+     * 
+     * @throws IOException
+     * @throws DataSealerException
+     */
+    @BeforeClass public void setupDataSealser() throws IOException, DataSealerException {
+        final File out = File.createTempFile("testDataSeal", "file");
+
+        final InputStream inStream = DataSealerTest.class.getResourceAsStream("/data/SealerKeyStore.jks");
+
+        final String keyStorePath = out.getAbsolutePath();
+
+        final OutputStream outStream = new FileOutputStream(out, false);
+
+        final byte buffer[] = new byte[1024];
+
+        final int bytesRead = inStream.read(buffer);
+        outStream.write(buffer, 0, bytesRead);
+        outStream.close();
+
+        dataSealer = new DataSealer();
+        dataSealer.setCipherKeyAlias("secret");
+        dataSealer.setCipherKeyPassword("kpassword");
+
+        dataSealer.setMacKeyAlias("secret");
+        dataSealer.setMacKeyPassword("kpassword");
+
+        dataSealer.setKeystorePassword("password");
+        dataSealer.setKeystorePath(keyStorePath);
+
+        dataSealer.init();
+
+    }
+
+    @Test public void testSetterGetters() throws ComponentInitializationException {
+        CryptoTransientIdAttributeDefinition defn = new CryptoTransientIdAttributeDefinition();
+        defn.setId(ID);
+        Assert.assertEquals(defn.getIdLifetime(), 0);
+        Assert.assertNull(defn.getDataSealer());
+        try {
+            defn.initialize();
+            Assert.fail("null dataSealer");
+        } catch (ComponentInitializationException e) {
+            // OK
+        }
+        defn.setDataSealer(dataSealer);
+        defn.initialize();
+        Assert.assertEquals(defn.getIdLifetime(), 4 * 1000 * 3600);
+        Assert.assertEquals(defn.getDataSealer(), dataSealer);
+
+        defn = new CryptoTransientIdAttributeDefinition();
+        defn.setId(ID);
+        defn.setDataSealer(dataSealer);
+        defn.setIdLifetime(TIMEOUT);
+        Assert.assertEquals(defn.getIdLifetime(), TIMEOUT);
+        Assert.assertEquals(defn.getDataSealer(), dataSealer);
+    }
+
+    @Test public void testEncode() throws ComponentInitializationException, ResolutionException, DataSealerException,
+            InterruptedException {
+        final CryptoTransientIdAttributeDefinition defn = new CryptoTransientIdAttributeDefinition();
+        defn.setId(ID);
+        defn.setDataSealer(dataSealer);
+        defn.setIdLifetime(TIMEOUT);
+        defn.initialize();
+
+        final AttributeResolutionContext context =
+                TestSources.createResolutionContext(TestSources.PRINCIPAL_ID, TestSources.IDP_ENTITY_ID,
+                        TestSources.SP_ENTITY_ID);
+
+        final Optional<Attribute> result = defn.doAttributeDefinitionResolve(context);
+
+        final Set<AttributeValue> values = result.get().getValues();
+        Assert.assertEquals(values.size(), 1);
+        final String code = ((StringAttributeValue) values.iterator().next()).getValue();
+
+        final String decode = dataSealer.unwrap(code);
+
+        Assert.assertEquals(decode, TestSources.IDP_ENTITY_ID + "!" + TestSources.SP_ENTITY_ID + "!"
+                + TestSources.PRINCIPAL_ID);
+
+        Thread.sleep(TIMEOUT);
+        try {
+            dataSealer.unwrap(code);
+            Assert.fail("Timeout not set correctly");
+        } catch (Exception e) {
+            // OK
+        }
+    }
+}
