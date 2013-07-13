148204bf17446e2177d492d57f1a6c985ecdc225
Rod Widdowson <rdw@steadingsoftware.com>
2013-07-13 09:00:28 +0000

    IDP-287 Change local variable from "matcher" to policy rule.  Got missed when I cloned the test from the scripted matcher test,

--- idp-attribute-filter-impl/src/test/java/net/shibboleth/idp/attribute/filter/impl/policyrule/ScriptedPolicyRuleTest.java
+++ idp-attribute-filter-impl/src/test/java/net/shibboleth/idp/attribute/filter/impl/policyrule/ScriptedPolicyRuleTest.java
@@ -20,7 +20,6 @@ package net.shibboleth.idp.attribute.filter.impl.policyrule;
 import javax.annotation.concurrent.ThreadSafe;
 
 import net.shibboleth.idp.attribute.filter.AttributeFilterContext;
-import net.shibboleth.idp.attribute.filter.AttributeFilterException;
 import net.shibboleth.idp.attribute.filter.PolicyRequirementRule.Tristate;
 import net.shibboleth.idp.attribute.filter.impl.matcher.AbstractMatcherPolicyRuleTest;
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
@@ -65,12 +64,12 @@ public class ScriptedPolicyRuleTest extends AbstractMatcherPolicyRuleTest {
     }
 
     @Test public void testNullArguments() throws Exception {
-        ScriptedPolicyRule matcher = new ScriptedPolicyRule(trueReturnScript);
-        matcher.setId("Test");
-        matcher.initialize();
+        ScriptedPolicyRule rule = new ScriptedPolicyRule(trueReturnScript);
+        rule.setId("Test");
+        rule.initialize();
 
         try {
-            matcher.matches(null);
+            rule.matches(null);
             Assert.fail();
         } catch (ConstraintViolationException e) {
             // expected this
@@ -85,48 +84,48 @@ public class ScriptedPolicyRuleTest extends AbstractMatcherPolicyRuleTest {
     }
 
     @Test public void testNullReturnScript() throws Exception {
-        ScriptedPolicyRule matcher = new ScriptedPolicyRule(nullReturnScript);
-        matcher.setId("Test");
-        matcher.initialize();
+        ScriptedPolicyRule rule = new ScriptedPolicyRule(nullReturnScript);
+        rule.setId("Test");
+        rule.initialize();
 
-        Assert.assertEquals(matcher.matches(filterContext), Tristate.FAIL);
+        Assert.assertEquals(rule.matches(filterContext), Tristate.FAIL);
     }
 
     @Test public void testInvalidReturnObjectValue() throws Exception {
-        ScriptedPolicyRule matcher = new ScriptedPolicyRule(invalidReturnObjectScript);
-        matcher.setId("Test");
-        matcher.initialize();
+        ScriptedPolicyRule rule = new ScriptedPolicyRule(invalidReturnObjectScript);
+        rule.setId("Test");
+        rule.initialize();
 
-        Assert.assertEquals(matcher.matches(filterContext), Tristate.FAIL);
+        Assert.assertEquals(rule.matches(filterContext), Tristate.FAIL);
     }
 
-    @Test public void testInitTeardown() throws AttributeFilterException, ComponentInitializationException {
-        ScriptedPolicyRule matcher = new ScriptedPolicyRule(trueReturnScript);
+    @Test public void testInitTeardown() throws ComponentInitializationException {
+        ScriptedPolicyRule rule = new ScriptedPolicyRule(trueReturnScript);
 
         boolean thrown = false;
         try {
-            matcher.matches(filterContext);
+            rule.matches(filterContext);
         } catch (UninitializedComponentException e) {
             thrown = true;
         }
         Assert.assertTrue(thrown, "matches before init");
 
-        matcher.setId("Test");
-        matcher.initialize();
-        matcher.matches(filterContext);
+        rule.setId("Test");
+        rule.initialize();
+        rule.matches(filterContext);
 
         thrown = false;
         try {
-            matcher.setScript(trueReturnScript);
+            rule.setScript(trueReturnScript);
         } catch (UnmodifiableComponentException e) {
             thrown = true;
         }
 
-        matcher.destroy();
+        rule.destroy();
 
         thrown = false;
         try {
-            matcher.initialize();
+            rule.initialize();
         } catch (DestroyedComponentException e) {
             thrown = true;
         }
@@ -134,7 +133,7 @@ public class ScriptedPolicyRuleTest extends AbstractMatcherPolicyRuleTest {
 
         thrown = false;
         try {
-            matcher.matches(filterContext);
+            rule.matches(filterContext);
         } catch (DestroyedComponentException e) {
             thrown = true;
         }
@@ -143,40 +142,40 @@ public class ScriptedPolicyRuleTest extends AbstractMatcherPolicyRuleTest {
     }
 
     @Test public void testEqualsHashToString() {
-        ScriptedPolicyRule matcher = new ScriptedPolicyRule(trueReturnScript);
+        ScriptedPolicyRule rule = new ScriptedPolicyRule(trueReturnScript);
 
-        matcher.toString();
+        rule.toString();
 
-        Assert.assertFalse(matcher.equals(null));
-        Assert.assertTrue(matcher.equals(matcher));
-        Assert.assertFalse(matcher.equals(this));
+        Assert.assertFalse(rule.equals(null));
+        Assert.assertTrue(rule.equals(rule));
+        Assert.assertFalse(rule.equals(this));
 
         ScriptedPolicyRule other = new ScriptedPolicyRule(trueReturnScript);
 
-        Assert.assertTrue(matcher.equals(other));
-        Assert.assertEquals(matcher.hashCode(), other.hashCode());
+        Assert.assertTrue(rule.equals(other));
+        Assert.assertEquals(rule.hashCode(), other.hashCode());
 
         other = new ScriptedPolicyRule(nullReturnScript);
 
-        Assert.assertFalse(matcher.equals(other));
-        Assert.assertNotSame(matcher.hashCode(), other.hashCode());
+        Assert.assertFalse(rule.equals(other));
+        Assert.assertNotSame(rule.hashCode(), other.hashCode());
 
     }
     
-    @Test public void testPredicate() throws ComponentInitializationException, AttributeFilterException {
-        ScriptedPolicyRule matcher = new ScriptedPolicyRule(nullReturnScript);
-        matcher.setId("test");
-        matcher.initialize();
+    @Test public void testPredicate() throws ComponentInitializationException {
+        ScriptedPolicyRule rule = new ScriptedPolicyRule(nullReturnScript);
+        rule.setId("test");
+        rule.initialize();
 
-        matcher = new ScriptedPolicyRule(trueReturnScript);
-        matcher.setId("test");
-        matcher.initialize();
-        Assert.assertEquals(matcher.matches(filterContext), Tristate.TRUE);
+        rule = new ScriptedPolicyRule(trueReturnScript);
+        rule.setId("test");
+        rule.initialize();
+        Assert.assertEquals(rule.matches(filterContext), Tristate.TRUE);
         
-        matcher = new ScriptedPolicyRule(falseReturnScript);
-        matcher.setId("test");
-        matcher.initialize();
-        Assert.assertEquals(matcher.matches(filterContext), Tristate.FALSE);
+        rule = new ScriptedPolicyRule(falseReturnScript);
+        rule.setId("test");
+        rule.initialize();
+        Assert.assertEquals(rule.matches(filterContext), Tristate.FALSE);
 
     }
 
