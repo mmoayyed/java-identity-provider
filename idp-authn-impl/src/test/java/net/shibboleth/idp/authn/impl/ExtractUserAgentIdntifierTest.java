899e460e0ad27051cdce1d7c0e7a244392116321
Scott Cantor <cantor.2@osu.edu>
2013-07-29 14:57:59 +0000

    User agent string extraction / unit test

---
+++ idp-authn-impl/src/test/java/net/shibboleth/idp/authn/impl/ExtractUserAgentIdntifierTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.authn.impl;
+
+
+import net.shibboleth.idp.authn.AuthnEventIds;
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.authn.context.UserAgentContext;
+
+import org.opensaml.profile.ProfileException;
+import org.opensaml.profile.action.ActionTestingSupport;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/** {@link ExtractUserAgentIdentifier} unit test. */
+public class ExtractUserAgentIdntifierTest extends InitializeAuthenticationContextTest {
+    
+    private ExtractUserAgentIdentifier action; 
+    
+    @BeforeMethod public void setUp() throws Exception {
+        super.setUp();
+        
+        action = new ExtractUserAgentIdentifier();
+        action.initialize();
+    }
+    
+    @Test public void testNoServlet() throws Exception {
+        action.execute(prc);
+        
+        ActionTestingSupport.assertEvent(prc, AuthnEventIds.NO_CREDENTIALS);
+    }
+
+    @Test public void testMissingHeader() throws ProfileException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        prc.setHttpRequest(request);
+        
+        action.execute(prc);
+        ActionTestingSupport.assertEvent(prc, AuthnEventIds.NO_CREDENTIALS);
+    }
+
+    @Test public void testValidHeader() throws ProfileException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("User-Agent", "foo bar baz");
+        prc.setHttpRequest(request);
+        
+        action.execute(prc);
+        ActionTestingSupport.assertProceedEvent(prc);
+        AuthenticationContext authCtx = prc.getSubcontext(AuthenticationContext.class, false);
+        UserAgentContext uaCtx = authCtx.getSubcontext(UserAgentContext.class, false);
+        Assert.assertNotNull(uaCtx, "No UserAgentContext attached");
+        Assert.assertEquals(uaCtx.getIdentifier(), "foo bar baz");
+    }
+}
