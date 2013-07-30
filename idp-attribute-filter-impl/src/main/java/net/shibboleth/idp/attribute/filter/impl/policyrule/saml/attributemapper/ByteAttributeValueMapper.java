5eb12e7688707303ccad59cf9d9820efe605054b
Rod Widdowson <rdw@steadingsoftware.com>
2013-07-30 07:08:19 +0000

    typo

--- idp-attribute-filter-impl/src/main/java/net/shibboleth/idp/attribute/filter/impl/policyrule/saml/attributemapper/ByteAttributeValueMapper.java
+++ idp-attribute-filter-impl/src/main/java/net/shibboleth/idp/attribute/filter/impl/policyrule/saml/attributemapper/ByteAttributeValueMapper.java
@@ -46,6 +46,6 @@ public class ByteAttributeValueMapper extends BaseAttributeValueMapper {
 
     /** {@inheritDoc} */
     @Nonnull protected String getAttributeTypeName() {
-        return "Bas64Encoded";
+        return "Base64Encoded";
     }
 }
