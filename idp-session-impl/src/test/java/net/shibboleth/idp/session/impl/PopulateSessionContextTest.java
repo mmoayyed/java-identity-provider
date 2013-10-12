316d6be27e503c793cef1a03a2d3261ce44474b0
Scott Cantor <cantor.2@osu.edu>
2013-10-12 00:43:30 +0000

    Refactor test case with base class.

--- idp-session-impl/src/test/java/net/shibboleth/idp/session/impl/PopulateSessionContextTest.java
+++ idp-session-impl/src/test/java/net/shibboleth/idp/session/impl/PopulateSessionContextTest.java
@@ -22,65 +22,31 @@ import javax.servlet.http.Cookie;
 import net.shibboleth.idp.session.SessionException;
 import net.shibboleth.idp.session.context.SessionContext;
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
-import net.shibboleth.utilities.java.support.net.CookieManager;
 import net.shibboleth.utilities.java.support.net.HttpServletRequestResponseContext;
-import net.shibboleth.utilities.java.support.net.ThreadLocalHttpServletRequestProxy;
-import net.shibboleth.utilities.java.support.net.ThreadLocalHttpServletResponseProxy;
-import net.shibboleth.utilities.java.support.security.SecureRandomIdentifierGenerationStrategy;
 
 import org.opensaml.profile.ProfileException;
 import org.opensaml.profile.action.ActionTestingSupport;
 import org.opensaml.profile.context.ProfileRequestContext;
-import org.opensaml.storage.impl.MemoryStorageService;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /** {@link PopulateSessionContext} unit test. */
-public class PopulateSessionContextTest {
+public class PopulateSessionContextTest extends SessionManagerBaseTestCase {
     
-    private static MemoryStorageService storageService;
-    
-    private static StorageBackedSessionManager manager;
-
     private ProfileRequestContext prc;
     
     private PopulateSessionContext action;
-    
-    @BeforeClass public static void setUp() throws ComponentInitializationException {
-        storageService = new MemoryStorageService();
-        storageService.initialize();
-
-        CookieManager cookieManager = new CookieManager();
-        cookieManager.setHttpServletRequest(new ThreadLocalHttpServletRequestProxy());
-        cookieManager.setHttpServletResponse(new ThreadLocalHttpServletResponseProxy());
-        
-        manager = new StorageBackedSessionManager();
-        manager.setSessionTimeout(5000);
-        manager.setStorageService(storageService);
-        manager.setIDGenerator(new SecureRandomIdentifierGenerationStrategy());
-        manager.setHttpServletRequest(new ThreadLocalHttpServletRequestProxy());
-        manager.setHttpServletResponse(new ThreadLocalHttpServletResponseProxy());
-        manager.setCookieManager(cookieManager);
-        manager.initialize();
-    }
-    
-    @AfterClass public static void tearDown() {
-        manager.destroy();
-        storageService.destroy();
-    }
     
     @BeforeMethod public void setUpAction() throws ComponentInitializationException {
         prc = new ProfileRequestContext();
         
         action = new PopulateSessionContext();
-        action.setHttpServletRequest(new ThreadLocalHttpServletRequestProxy());
-        action.setHttpServletResponse(new ThreadLocalHttpServletResponseProxy());
-        action.setSessionResolver(manager);
+        action.setHttpServletRequest(requestProxy);
+        action.setHttpServletResponse(responseProxy);
+        action.setSessionResolver(sessionManager);
         action.initialize();
     }
     
@@ -92,7 +58,7 @@ public class PopulateSessionContextTest {
     }
     
     @Test public void testSession() throws ProfileException, SessionException {
-        Cookie cookie = createSession();
+        Cookie cookie = createSession("joe");
         
         HttpServletRequestResponseContext.loadCurrent(new MockHttpServletRequest(), new MockHttpServletResponse());
         ((MockHttpServletRequest) HttpServletRequestResponseContext.getRequest()).setCookies(cookie);
@@ -106,7 +72,7 @@ public class PopulateSessionContextTest {
     }
 
     @Test public void testAddressRebind() throws ProfileException, SessionException {
-        Cookie cookie = createSession();
+        Cookie cookie = createSession("joe");
         
         HttpServletRequestResponseContext.loadCurrent(new MockHttpServletRequest(), new MockHttpServletResponse());
         ((MockHttpServletRequest) HttpServletRequestResponseContext.getRequest()).setCookies(cookie);
@@ -121,7 +87,7 @@ public class PopulateSessionContextTest {
     }
     
     @Test public void testAddressMismatch() throws ProfileException, SessionException {
-        Cookie cookie = createSession();
+        Cookie cookie = createSession("joe");
         
         HttpServletRequestResponseContext.loadCurrent(new MockHttpServletRequest(), new MockHttpServletResponse());
         ((MockHttpServletRequest) HttpServletRequestResponseContext.getRequest()).setCookies(cookie);
@@ -133,7 +99,7 @@ public class PopulateSessionContextTest {
     }
 
     @Test public void testTimeout() throws ProfileException, SessionException, InterruptedException {
-        Cookie cookie = createSession();
+        Cookie cookie = createSession("joe");
         
         HttpServletRequestResponseContext.loadCurrent(new MockHttpServletRequest(), new MockHttpServletResponse());
         ((MockHttpServletRequest) HttpServletRequestResponseContext.getRequest()).setCookies(cookie);
@@ -143,19 +109,5 @@ public class PopulateSessionContextTest {
         action.execute(prc);
         ActionTestingSupport.assertProceedEvent(prc);
         Assert.assertNull(prc.getSubcontext(SessionContext.class, false));
-    }
-    
-    /**
-     * Creates a session and returns the cookie established by the SessionManager for it.
-     * 
-     * @return
-     * @throws SessionException
-     */
-    private Cookie createSession() throws SessionException {
-        HttpServletRequestResponseContext.loadCurrent(new MockHttpServletRequest(), new MockHttpServletResponse());
-        manager.createSession("joe");
-        Cookie cookie = ((MockHttpServletResponse) HttpServletRequestResponseContext.getResponse()).getCookies()[0];
-        HttpServletRequestResponseContext.clearCurrent();
-        return cookie;
     }
 }
