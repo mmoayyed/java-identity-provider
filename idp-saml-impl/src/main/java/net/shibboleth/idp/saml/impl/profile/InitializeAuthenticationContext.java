56e9f558c45a9cbb8ebdb12272e9523f23b8accd
Scott Cantor <cantor.2@osu.edu>
2013-12-27 03:57:12 +0000

    Move InitAuthnContext action into idp-saml-impl, support both SAML versions.

--- idp-saml-impl/src/main/java/net/shibboleth/idp/saml/impl/profile/InitializeAuthenticationContext.java
+++ idp-saml-impl/src/main/java/net/shibboleth/idp/saml/impl/profile/InitializeAuthenticationContext.java
@@ -15,70 +15,77 @@
  * limitations under the License.
  */
 
-package net.shibboleth.idp.profile.impl;
+package net.shibboleth.idp.saml.impl.profile;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import net.shibboleth.idp.authn.context.AuthenticationContext;
-import net.shibboleth.idp.profile.AbstractProfileAction;
-import net.shibboleth.idp.profile.ActionSupport;
 
 import org.opensaml.messaging.context.MessageContext;
 import org.opensaml.profile.ProfileException;
-import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.action.AbstractProfileAction;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.opensaml.saml.saml2.core.AuthnRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.webflow.execution.Event;
-import org.springframework.webflow.execution.RequestContext;
 
 /**
  * An action that creates an {@link AuthenticationContext} and attaches it to the current {@link ProfileRequestContext}.
  * 
+ * <p>If the incoming message is a SAML 2.0 {@link AuthnRequest}, then basic authentication policy (IsPassive,
+ * ForceAuthn) is copied into the context from the request.</p>
+ * 
  * @event {@link org.opensaml.profile.action.EventIds#PROCEED_EVENT_ID}
- * @event {@link org.opensaml.profile.action.EventIds#INVALID_MSG_CTX}
+ * @post ProfileRequestContext.getSubcontext(AuthenticationContext.class, false) != true
+ * @post SAML 2.0 AuthnRequest policy flags are copied to the {@link AuthenticationContext}
  */
-public class CreateAuthenticationContext extends AbstractProfileAction {
+public class InitializeAuthenticationContext extends AbstractProfileAction {
 
     /** Class logger. */
-    private final Logger log = LoggerFactory.getLogger(CreateAuthenticationContext.class);
+    @Nonnull private final Logger log = LoggerFactory.getLogger(InitializeAuthenticationContext.class);
+
+    /** Incoming SAML 2.0 request, if present. */
+    @Nullable private AuthnRequest authnRequest;
 
     /** {@inheritDoc} */
-    @Override @Nonnull protected Event doExecute(@Nonnull final RequestContext springRequestContext,
-            @Nonnull final ProfileRequestContext profileRequestContext) throws ProfileException {
-
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext) throws ProfileException {
         final MessageContext<?> msgCtx = profileRequestContext.getInboundMessageContext();
         if (msgCtx == null) {
             log.debug("{} No inbound message context", getLogPrefix());
-            return ActionSupport.buildEvent(this, EventIds.INVALID_MSG_CTX);
+            return super.doPreExecute(profileRequestContext);
         }
 
         final Object message = msgCtx.getMessage();
         if (message == null) {
             log.debug("{} No inbound message", getLogPrefix());
-            return ActionSupport.buildEvent(this, EventIds.INVALID_MSG_CTX);
+            return super.doPreExecute(profileRequestContext);
         }
 
-        // TODO tz This action should be profile generic, not SAML 2 specific.
-        
         if (!(message instanceof AuthnRequest)) {
             log.debug("{} Inbound message was not a SAML 2 AuthnRequest", getLogPrefix());
-            return ActionSupport.buildEvent(this, EventIds.INVALID_MSG_CTX);
+            return super.doPreExecute(profileRequestContext);
         }
 
-        final AuthnRequest request = (AuthnRequest) message;
+        authnRequest = (AuthnRequest) message;
+        
+        return super.doPreExecute(profileRequestContext);
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) throws ProfileException {
 
         AuthenticationContext authnCtx = new AuthenticationContext();
-        authnCtx.setForceAuthn(request.isForceAuthn());
-        authnCtx.setIsPassive(request.isPassive());
-
-        // TODO set other properties, etc.
+        
+        if (authnRequest != null) {
+            authnCtx.setForceAuthn(authnRequest.isForceAuthn());
+            authnCtx.setIsPassive(authnRequest.isPassive());
+        }
 
         profileRequestContext.addSubcontext(authnCtx, true);
 
         log.debug("{} Created authentication context {}", getLogPrefix(), authnCtx);
-
-        return getResult(this, profileRequestContext);
     }
 }
---
+++ idp-saml-impl/src/test/java/net/shibboleth/idp/saml/impl/profile/InitializeAuthenticationContextTest.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.saml.impl.profile;
+
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.saml.impl.profile.InitializeAuthenticationContext;
+
+import org.opensaml.profile.RequestContextBuilder;
+import org.opensaml.profile.action.ActionTestingSupport;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.saml.saml2.core.AuthnRequest;
+import org.opensaml.saml.saml2.core.impl.AuthnRequestBuilder;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/** {@link InitializeAuthenticationContext} unit test. */
+public class InitializeAuthenticationContextTest {
+
+    /** Test that the action functions properly if there is no inbound message context. */
+    @Test public void testNoInboundMessageContext() throws Exception {
+        ProfileRequestContext profileCtx = new ProfileRequestContext();
+
+        InitializeAuthenticationContext action = new InitializeAuthenticationContext();
+        action.setId("test");
+        action.initialize();
+
+        action.execute(profileCtx);
+        ActionTestingSupport.assertProceedEvent(profileCtx);
+
+        AuthenticationContext authnCtx = profileCtx.getSubcontext(AuthenticationContext.class);
+        Assert.assertNotNull(authnCtx);
+        Assert.assertFalse(authnCtx.isForceAuthn());
+        Assert.assertFalse(authnCtx.isPassive());
+    }
+
+    /** Test that the action functions properly if there is no inbound message. */
+    @Test public void testNoInboundMessage() throws Exception {
+        ProfileRequestContext profileCtx =
+                new RequestContextBuilder().setInboundMessage(null).buildProfileRequestContext();
+
+        InitializeAuthenticationContext action = new InitializeAuthenticationContext();
+        action.setId("test");
+        action.initialize();
+
+        action.execute(profileCtx);
+        ActionTestingSupport.assertProceedEvent(profileCtx);
+
+        AuthenticationContext authnCtx = profileCtx.getSubcontext(AuthenticationContext.class);
+        Assert.assertNotNull(authnCtx);
+        Assert.assertFalse(authnCtx.isForceAuthn());
+        Assert.assertFalse(authnCtx.isPassive());
+    }
+
+    /** Test that the action functions properly if the inbound message is not a SAML 2 AuthnRequest. */
+    @Test public void testSAML1AuthnRequest() throws Exception {
+        ProfileRequestContext profileCtx =
+                new RequestContextBuilder().setInboundMessage(
+                        new IdPInitatedSSORequest("https://sp.example.org/sp", null, null, null)
+                        ).buildProfileRequestContext();
+
+        InitializeAuthenticationContext action = new InitializeAuthenticationContext();
+        action.setId("test");
+        action.initialize();
+
+        action.execute(profileCtx);
+        ActionTestingSupport.assertProceedEvent(profileCtx);
+
+        AuthenticationContext authnCtx = profileCtx.getSubcontext(AuthenticationContext.class);
+        Assert.assertNotNull(authnCtx);
+        Assert.assertFalse(authnCtx.isForceAuthn());
+        Assert.assertFalse(authnCtx.isPassive());
+    }
+
+    /** Test that the action proceeds properly if the inbound message is a SAML2 AuthnRequest. */
+    @Test public void testCreateAuthenticationContext() throws Exception {
+        AuthnRequest authnRequest = new AuthnRequestBuilder().buildObject();
+        authnRequest.setIsPassive(true);
+        authnRequest.setForceAuthn(true);
+
+        ProfileRequestContext profileCtx =
+                new RequestContextBuilder().setInboundMessage(authnRequest).buildProfileRequestContext();
+
+        InitializeAuthenticationContext action = new InitializeAuthenticationContext();
+        action.setId("test");
+        action.initialize();
+
+        action.execute(profileCtx);
+        ActionTestingSupport.assertProceedEvent(profileCtx);
+        
+        AuthenticationContext authnCtx = profileCtx.getSubcontext(AuthenticationContext.class);
+        Assert.assertNotNull(authnCtx);
+        Assert.assertTrue(authnCtx.isForceAuthn());
+        Assert.assertTrue(authnCtx.isPassive());
+    }
+
+}
