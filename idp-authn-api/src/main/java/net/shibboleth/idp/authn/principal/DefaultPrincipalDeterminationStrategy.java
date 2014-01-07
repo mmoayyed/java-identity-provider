114ac3ec5e4b61282f7ad8a4e6908a9fcd7a70c2
Scott Cantor <cantor.2@osu.edu>
2014-01-07 16:43:03 +0000

    Functor to simplify custom Principal extraction for profile fulfillment

---
+++ idp-authn-api/src/main/java/net/shibboleth/idp/authn/principal/DefaultPrincipalDeterminationStrategy.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.authn.principal;
+
+import java.security.Principal;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+
+import org.opensaml.messaging.context.navigate.ChildContextLookup;
+import org.opensaml.profile.context.ProfileRequestContext;
+
+import com.google.common.base.Function;
+
+/**
+ * Function that returns the first custom {@link Principal} of a particular type found on the
+ * {@link AuthenticationResult} returned by {@link AuthenticationContext#getAuthenticationResult()}.
+ * 
+ * <p>The context is located using a lookup strategy, by default a child of the input context.<p>
+ * 
+ * <p>If for any reason a matching Principal can't be located, a default is returned.</p>
+ * 
+ * @param <T>   the custom Principal type to locate
+ */
+public class DefaultPrincipalDeterminationStrategy<T extends Principal> implements Function<ProfileRequestContext, T> {
+    
+    /** Authentication context lookup strategy. */
+    @Nonnull private Function<ProfileRequestContext, AuthenticationContext> authnContextLookupStrategy;
+    
+    /** Type of Principal to return. */
+    @Nonnull private Class<T> principalType;
+    
+    /** Default Principal to return. */
+    @Nonnull private T defaultPrincipal;
+    
+    /**
+     * Constructor.
+     * 
+     * @param type class type for Principal type
+     * @param principal default Principal to return
+     */
+    public DefaultPrincipalDeterminationStrategy(@Nonnull final Class<T> type, @Nonnull final T principal) {
+        principalType = Constraint.isNotNull(type, "Class type cannot be null");
+        defaultPrincipal = Constraint.isNotNull(principal, "Default Principal cannot be null");
+        authnContextLookupStrategy = new ChildContextLookup<>(AuthenticationContext.class, false);
+    }
+    
+    /**
+     * Set lookup strategy for {@link AuthenticationContext}.
+     * 
+     * @param strategy  lookup strategy
+     */
+    public synchronized void setAuthenticationContextLookupStrategy(
+            @Nonnull final Function<ProfileRequestContext, AuthenticationContext> strategy) {
+        authnContextLookupStrategy = Constraint.isNotNull(strategy, "Lookup strategy cannot be null");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @Nullable public T apply(@Nullable final ProfileRequestContext input) {
+        final AuthenticationContext ac = authnContextLookupStrategy.apply(input);
+        if (ac == null || ac.getAuthenticationResult() == null) {
+            return defaultPrincipal;
+        }
+        
+        final Set<T> principals = ac.getAuthenticationResult().getSupportedPrincipals(principalType);
+        if (principals.isEmpty()) {
+            return defaultPrincipal;
+        } else {
+            return principals.iterator().next();
+        }
+    }
+
+}
