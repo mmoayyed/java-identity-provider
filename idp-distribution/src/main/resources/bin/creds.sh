4ffc7a27c5bddb036a8e434ae6cf80503e75a469
Tom Zeller <tzeller@dragonacea.biz>
2014-07-23 03:52:57 +0000

    IDP-436 Move runtime dependencies for generating credentials to bin/lib rather than war/WEB-INF/lib.

--- idp-distribution/src/main/resources/bin/creds.sh
+++ idp-distribution/src/main/resources/bin/creds.sh
@@ -10,11 +10,11 @@ URI_ALT_NAME=https://idp.example.org/idp/shibboleth
 
 CLASS=net.shibboleth.utilities.java.support.security.SelfSignedCertificateGenerator
 
-java -cp "war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --keyfile creds/idp-signing.key --certfile creds/idp-signing.crt --uriAltName $URI_ALT_NAME
+java -cp "bin/lib/*:war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --keyfile creds/idp-signing.key --certfile creds/idp-signing.crt --uriAltName $URI_ALT_NAME
 
-java -cp "war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --keyfile creds/idp-encryption.key --certfile creds/idp-encryption.crt --uriAltName $URI_ALT_NAME
+java -cp "bin/lib/*:war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --keyfile creds/idp-encryption.key --certfile creds/idp-encryption.crt --uriAltName $URI_ALT_NAME
 
-java -cp "war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --storefile creds/idp-tls.p12 --storepass changeit --uriAltName $URI_ALT_NAME
+java -cp "bin/lib/*:war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --storefile creds/idp-tls.p12 --storepass changeit --uriAltName $URI_ALT_NAME
 
 # TODO generate creds/sealer.jks
 keytool -genseckey -alias idpSecretKey -keypass password -storepass password -storetype JCEKS -keyalg AES -keysize 256 -keystore creds/sealer.jks
