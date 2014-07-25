3ef2c408816ed90a8c5ea03b84708198f04de44c
Tom Zeller <tzeller@dragonacea.biz>
2014-07-25 19:16:41 +0000

    Revert generating credentials using idp.properties via Spring.

--- idp-distribution/src/main/resources/bin/creds.bat
+++ idp-distribution/src/main/resources/bin/creds.bat
@@ -2,9 +2,17 @@
 
 rem This is a rudimentary script to generate credentials for the IdP.
 rem Run from within the idp.home directory as bin\creds.bat.
-rem See conf\generate-credentials.xml to customize.
 
-java -cp "bin\lib\*;war\WEB-INF\lib\*" net.shibboleth.idp.GenerateCredentials
+set HOSTNAME=idp.example.org
 
-rem TODO generate creds/sealer.jks with parameters from conf/idp.properties
+set URI_ALT_NAME=https://idp.example.org/idp/shibboleth
+
+set CLASS=net.shibboleth.utilities.java.support.security.SelfSignedCertificateGenerator
+
+java -cp "bin\lib\*;war\WEB-INF\lib\*" %CLASS% --hostname %HOSTNAME% --keyfile creds/idp-signing.key --certfile creds/idp-signing.crt --uriAltName %URI_ALT_NAME%
+
+java -cp "bin\lib\*;war\WEB-INF\lib\*" %CLASS% --hostname %HOSTNAME% --keyfile creds/idp-encryption.key --certfile creds/idp-encryption.crt --uriAltName %URI_ALT_NAME%
+
+java -cp "bin\lib\*;war\WEB-INF\lib\*" %CLASS% --hostname %HOSTNAME% --storefile creds/idp-tls.p12 --storepass changeit --uriAltName %URI_ALT_NAME%
+
 keytool -genseckey -alias idpSecretKey -keypass password -storepass password -storetype JCEKS -keyalg AES -keysize 256 -keystore creds/sealer.jks
--- idp-distribution/src/main/resources/bin/creds.sh
+++ idp-distribution/src/main/resources/bin/creds.sh
@@ -2,9 +2,17 @@
 
 # This is a rudimentary script to generate credentials for the IdP.
 # Run from within the idp.home directory as bin/creds.sh.
-# See conf/generate-credentials.xml to customize.
 
-java -cp "bin/lib/*:war/WEB-INF/lib/*" net.shibboleth.idp.GenerateCredentials
+HOSTNAME=idp.example.org
 
-# TODO generate creds/sealer.jks with parameters from conf/idp.properties
+URI_ALT_NAME=https://idp.example.org/idp/shibboleth
+
+CLASS=net.shibboleth.utilities.java.support.security.SelfSignedCertificateGenerator
+
+java -cp "bin/lib/*:war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --keyfile creds/idp-signing.key --certfile creds/idp-signing.crt --uriAltName $URI_ALT_NAME
+
+java -cp "bin/lib/*:war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --keyfile creds/idp-encryption.key --certfile creds/idp-encryption.crt --uriAltName $URI_ALT_NAME
+
+java -cp "bin/lib/*:war/WEB-INF/lib/*" $CLASS --hostname $HOSTNAME --storefile creds/idp-tls.p12 --storepass changeit --uriAltName $URI_ALT_NAME
+
 keytool -genseckey -alias idpSecretKey -keypass password -storepass password -storetype JCEKS -keyalg AES -keysize 256 -keystore creds/sealer.jks
