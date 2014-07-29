a133d6c7ed286af164a1b9942ed05e94d4f27299
Rod Widdowson <rdw@steadingsoftware.com>
2014-07-29 08:58:28 +0000

    IDP-447 Add scripted data connector and test.

---
+++ idp-attribute-resolver-impl/src/test/resources/data/net/shibboleth/idp/attribute/resolver/impl/dc/scriptedConnector.js
@@ -0,0 +1,21 @@
+importPackage(Packages.net.shibboleth.idp.attribute);
+importPackage(Packages.java.util);
+importPackage(Packages.java.lang);
+
+attr = new IdPAttribute("ScriptedOne");
+set = new HashSet(2);
+set.add(new StringAttributeValue("Value 1"));
+set.add(new StringAttributeValue("Value 2"));
+attr.setValues(set);
+connectorResults.add(attr);
+
+attr = new IdPAttribute("TwoScripted");
+set = new HashSet(4);
+set.add(new StringAttributeValue("1Value"));
+set.add(new StringAttributeValue("2Value"));
+set.add(new Integer(4));
+set.add(new StringAttributeValue("3Value"));
+attr.setValues(set);
+connectorResults.add(attr);
+
+connectorResults.add(new Integer(4));
