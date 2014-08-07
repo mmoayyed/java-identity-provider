cbbd8428c79c1d20af58a6ddf985568248d95824
Rod Widdowson <rdw@steadingsoftware.com>
2014-08-07 16:46:03 +0000

    IDP-463 Warn where there is an implicit setId.  Name Beans, not the components.

--- idp-session-impl/src/main/java/net/shibboleth/idp/session/impl/StorageBackedSessionManager.java
+++ idp-session-impl/src/main/java/net/shibboleth/idp/session/impl/StorageBackedSessionManager.java
@@ -176,6 +176,7 @@ public class StorageBackedSessionManager extends AbstractIdentifiableInitializab
         consistentAddress = true;
         cookieName = DEFAULT_COOKIE_NAME;
 
+        // TODO(RDW) Implict setId
         setId(UUID.randomUUID().toString());
     }
 
