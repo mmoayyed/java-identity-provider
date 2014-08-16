5745544ffee66ea7424d7b389477d16a1b8a805f
Scott Cantor <cantor.2@osu.edu>
2014-08-16 02:53:58 +0000

    IDP-375 - initial set of assertion audit extractors

---
+++ idp-saml-impl/src/main/java/net/shibboleth/idp/saml/audit/impl/SessionIndexAuditExtractor.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.saml.audit.impl;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.saml.common.SAMLObject;
+import org.opensaml.saml.saml2.core.ArtifactResponse;
+import org.opensaml.saml.saml2.core.Assertion;
+import org.opensaml.saml.saml2.core.AuthnStatement;
+import org.opensaml.saml.saml2.core.Response;
+
+import com.google.common.base.Function;
+
+import net.shibboleth.idp.profile.AuditExtractorFunction;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+
+/** {@link AuditExtractorFunction} that returns the first SessionIndex from an assertions in a response. */
+public class SessionIndexAuditExtractor implements AuditExtractorFunction {
+
+    /** Lookup strategy for message to read from. */
+    @Nonnull private final Function<ProfileRequestContext,SAMLObject> responseLookupStrategy;
+    
+    /**
+     * Constructor.
+     *
+     * @param strategy lookup strategy for message
+     */
+    public SessionIndexAuditExtractor(@Nonnull final Function<ProfileRequestContext,SAMLObject> strategy) {
+        responseLookupStrategy = Constraint.isNotNull(strategy, "Response lookup strategy cannot be null");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @Nullable public Collection<String> apply(@Nullable final ProfileRequestContext input) {
+        SAMLObject response = responseLookupStrategy.apply(input);
+        if (response != null) {
+            
+            // Step down into ArtifactResponses.
+            if (response instanceof ArtifactResponse) {
+                response = ((ArtifactResponse) response).getMessage();
+            }
+            
+            if (response instanceof Response) {
+                for (final Assertion assertion : ((Response) response).getAssertions()) {
+                    for (final AuthnStatement statement : assertion.getAuthnStatements()) {
+                        if (statement.getSessionIndex() != null) {
+                            return Collections.singletonList(statement.getSessionIndex());
+                        }
+                    }
+                }
+            }
+        }
+        
+        return Collections.emptyList();
+    }
+    
+}
