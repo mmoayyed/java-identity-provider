47bb40e66c317d2376200d00e69705a9a6b87739
Rod Widdowson <rdw@steadingsoftware.com>
2014-10-18 14:37:19 +0000

    IDP-357 Revert last two.  The initial code handles the Immutable case by working backwards from V2... sigh

--- idp-attribute-resolver-impl/src/main/java/net/shibboleth/idp/attribute/resolver/impl/AttributeResolverImpl.java
+++ idp-attribute-resolver-impl/src/main/java/net/shibboleth/idp/attribute/resolver/impl/AttributeResolverImpl.java
@@ -419,7 +419,6 @@ public class AttributeResolverImpl extends AbstractServiceableComponent<Attribut
                 if (!monitor.add(value)) {
                     log.debug("{} Removing duplicate value {} of attribute {} from resolution result", logPrefix, value,
                             resolvedAttribute.getId());
-                    valueIter.remove();
                 }
             }
 
