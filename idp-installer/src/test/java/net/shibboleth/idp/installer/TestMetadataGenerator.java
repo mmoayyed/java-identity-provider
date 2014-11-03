e193afbb03b883ef067e1a65cf6abacc4f0b5516
Rod Widdowson <rdw@steadingsoftware.com>
2014-11-03 16:32:13 +0000

    IDP-481 Comment out SAML2 AA endpoint.  Fix (and extend) test

--- idp-installer/src/test/java/net/shibboleth/idp/installer/TestMetadataGenerator.java
+++ idp-installer/src/test/java/net/shibboleth/idp/installer/TestMetadataGenerator.java
@@ -77,6 +77,47 @@ public class TestMetadataGenerator extends XMLObjectBaseTestCase {
             Assert.assertNotNull(aa);
             Assert.assertSame(entity.getAttributeAuthorityDescriptor(SAMLConstants.SAML20P_NS), aa);
             Assert.assertNull(entity.getAttributeAuthorityDescriptor("urn:mace:shibboleth:1.0"));
+            Assert.assertEquals(aa.getAttributeServices().size(), 1);
+            exts = aa.getExtensions().getUnknownXMLObjects();
+            Assert.assertEquals(exts.size(), 1);
+            Assert.assertEquals(aa.getKeyDescriptors().size(), 3);
+        } finally {
+            out.delete();
+        }
+    }
+
+    @Test public void withSaml2AQ() throws IOException {
+        final MetadataGeneratorTask task = new MetadataGeneratorTask();
+        final Resource backChannelCrt = new ClassPathResource("/credentials/idp-backchannel.crt");
+        task.setBackchannelCert(backChannelCrt.getFile());
+        final File out = File.createTempFile("TestMetadataGenerator", ".xml");
+
+        try {
+            task.setBackchannelCert(backChannelCrt.getFile());
+            task.setDnsName("idp.example.org");
+            task.setSAML2AttributeQueryCommented(false);
+            task.setOutput(out);
+
+            task.execute();
+
+            final EntityDescriptor entity = (EntityDescriptor) unmarshallElement(out.getAbsolutePath());
+            final IDPSSODescriptor idpsso = entity.getIDPSSODescriptor(SAMLConstants.SAML20P_NS);
+            Assert.assertNotNull(idpsso);
+            Assert.assertSame(entity.getIDPSSODescriptor(SAMLConstants.SAML11P_NS), idpsso);
+            Assert.assertSame(entity.getIDPSSODescriptor("urn:mace:shibboleth:1.0"), idpsso);
+            Assert.assertNull(entity.getIDPSSODescriptor("urn:mace:shibboleth:1.0:nameid"));
+            Assert.assertEquals(idpsso.getArtifactResolutionServices().size(), 2);
+            Assert.assertEquals(idpsso.getSingleLogoutServices().size(), 3);
+            Assert.assertEquals(idpsso.getSingleSignOnServices().size(), 4);
+            Assert.assertEquals(idpsso.getNameIDFormats().size(), 2);
+            List<XMLObject> exts = idpsso.getExtensions().getUnknownXMLObjects();
+            Assert.assertEquals(exts.size(), 1);
+            Assert.assertEquals(idpsso.getKeyDescriptors().size(), 3);
+
+            final AttributeAuthorityDescriptor aa = entity.getAttributeAuthorityDescriptor(SAMLConstants.SAML11P_NS);
+            Assert.assertNotNull(aa);
+            Assert.assertSame(entity.getAttributeAuthorityDescriptor(SAMLConstants.SAML20P_NS), aa);
+            Assert.assertNull(entity.getAttributeAuthorityDescriptor("urn:mace:shibboleth:1.0"));
             Assert.assertEquals(aa.getAttributeServices().size(), 2);
             exts = aa.getExtensions().getUnknownXMLObjects();
             Assert.assertEquals(exts.size(), 1);
