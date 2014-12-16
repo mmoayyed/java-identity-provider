2710835cdab3812b1ff2e434d4aaed102a8740f2
Tom Zeller <tzeller@dragonacea.biz>
2014-12-16 02:03:09 +0000

    IDP-499 Delete first try at secondary index implementation.

--- idp-consent/src/test/java/net/shibboleth/idp/consent/ConsentTestingSupport.java
+++ idp-consent/src/test/java/net/shibboleth/idp/consent/ConsentTestingSupport.java
@@ -26,7 +26,6 @@ import net.shibboleth.idp.attribute.IdPAttribute;
 import net.shibboleth.idp.attribute.IdPAttributeValue;
 import net.shibboleth.idp.attribute.StringAttributeValue;
 import net.shibboleth.idp.consent.storage.ConsentResult;
-import net.shibboleth.idp.consent.storage.StorageIndex;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -70,22 +69,6 @@ public class ConsentTestingSupport {
         map.put(attribute2.getId(), attribute2);
         map.put(attribute3.getId(), attribute3);
         return map;
-    }
-
-    public static final Map<String, StorageIndex> newStorageIndexMap() {
-        final StorageIndex storageIndex1 = new StorageIndex();
-        storageIndex1.setContext("context1");
-        storageIndex1.getKeys().add("key1");
-
-        final StorageIndex storageIndex2 = new StorageIndex();
-        storageIndex2.setContext("context2");
-        storageIndex2.getKeys().add("key1");
-        storageIndex2.getKeys().add("key2");
-
-        final Map<String, StorageIndex> storageIndexes = new HashMap<>();
-        storageIndexes.put(storageIndex1.getContext(), storageIndex1);
-        storageIndexes.put(storageIndex2.getContext(), storageIndex2);
-        return storageIndexes;
     }
 
     public static final List<ConsentResult> newConsentResults() {
