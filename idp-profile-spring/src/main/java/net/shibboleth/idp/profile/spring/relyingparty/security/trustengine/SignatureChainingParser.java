99ddde3f081c5bba63a6826467e7113c5e3bbd3c
Rod Widdowson <rdw@steadingsoftware.com>
2015-02-22 15:00:49 +0000

    IDP-620 TrustEngineRef takes a ref attribute, not text content

--- idp-profile-spring/src/main/java/net/shibboleth/idp/profile/spring/relyingparty/security/trustengine/SignatureChainingParser.java
+++ idp-profile-spring/src/main/java/net/shibboleth/idp/profile/spring/relyingparty/security/trustengine/SignatureChainingParser.java
@@ -17,54 +17,22 @@
 
 package net.shibboleth.idp.profile.spring.relyingparty.security.trustengine;
 
-import java.util.List;
-
 import javax.xml.namespace.QName;
 
-import net.shibboleth.ext.spring.util.SpringSupport;
 import net.shibboleth.idp.profile.spring.relyingparty.security.SecurityNamespaceHandler;
-import net.shibboleth.utilities.java.support.primitive.StringSupport;
-import net.shibboleth.utilities.java.support.xml.ElementSupport;
 
-import org.springframework.beans.BeanMetadataElement;
-import org.springframework.beans.factory.config.RuntimeBeanReference;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.ManagedList;
-import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 /**
  * Parser for trust engines of type SignatureChaining.
  */
-public class SignatureChainingParser extends AbstractTrustEngineParser {
+public class SignatureChainingParser extends AbstractChainingParser {
     
     /** Schema type. */
     public static final QName TYPE_NAME = new QName(SecurityNamespaceHandler.NAMESPACE, "SignatureChaining");
 
-    /** TrustEngineRef element name. */
-    public static final QName TRUST_ENGINE_REF= new QName(SecurityNamespaceHandler.NAMESPACE, "TrustEngineRef");
-   
     /** {@inheritDoc} */
     @Override protected Class<?> getBeanClass(Element element) {
         return ChainingSignatureTrustEngineFactory.class;
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
-        super.doParse(element, parserContext, builder);
-
-        final List<Element> childEngines =
-                ElementSupport.getChildElements(element, SecurityNamespaceHandler.TRUST_ENGINE_ELEMENT_NAME);
-        final List<Element> childEngineRefs =
-                ElementSupport.getChildElements(element, TRUST_ENGINE_REF);
-        
-        final List<BeanMetadataElement> allChildren = new ManagedList<>(childEngines.size()+ childEngineRefs.size());
-        
-        allChildren.addAll(SpringSupport.parseCustomElements(childEngines, parserContext));
-        
-        for (Element ref:childEngineRefs) {
-            allChildren.add(new RuntimeBeanReference(StringSupport.trim(ref.getTextContent())));
-        }
-        builder.addConstructorArgValue(allChildren);
     }
 }
