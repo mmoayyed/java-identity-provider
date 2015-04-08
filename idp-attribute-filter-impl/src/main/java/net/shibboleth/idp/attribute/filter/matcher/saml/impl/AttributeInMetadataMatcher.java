7d7d720ea26aeddcb42e1d2275af080275ac3e3c
Rod Widdowson <rdw@steadingsoftware.com>
2015-04-08 08:53:43 +0000

    IDP-685 Set the default value of onlyIfRequired to be true

--- idp-attribute-filter-impl/src/main/java/net/shibboleth/idp/attribute/filter/matcher/saml/impl/AttributeInMetadataMatcher.java
+++ idp-attribute-filter-impl/src/main/java/net/shibboleth/idp/attribute/filter/matcher/saml/impl/AttributeInMetadataMatcher.java
@@ -68,7 +68,7 @@ public class AttributeInMetadataMatcher extends AbstractIdentifiableInitializabl
     @Nonnull private final Logger log = LoggerFactory.getLogger(AttributeInMetadataMatcher.class);
 
     /** Whether optionally requested attributes should be matched. */
-    private boolean onlyIfRequired;
+    private boolean onlyIfRequired = true;
 
     /** Whether to return a match if the metadata does not contain an ACS descriptor. */
     private boolean matchIfMetadataSilent;
