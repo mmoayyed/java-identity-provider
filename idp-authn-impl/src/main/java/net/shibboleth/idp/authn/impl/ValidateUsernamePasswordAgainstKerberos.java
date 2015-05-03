49447cfa6e9f6ef873f8a3110097af193c76a220
Scott Cantor <cantor.2@osu.edu>
2015-05-03 00:20:31 +0000

    IDP-114 - rewrite of Kerberos action on top of JAAS module and GSS exchange

--- idp-authn-impl/src/main/java/net/shibboleth/idp/authn/impl/ValidateUsernamePasswordAgainstKerberos.java
+++ idp-authn-impl/src/main/java/net/shibboleth/idp/authn/impl/ValidateUsernamePasswordAgainstKerberos.java
@@ -17,38 +17,45 @@
 
 package net.shibboleth.idp.authn.impl;
 
-import java.io.File;
-import java.io.IOException;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.security.auth.Subject;
-import javax.security.auth.kerberos.KerberosPrincipal;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
 
 import net.shibboleth.idp.authn.AbstractValidationAction;
 import net.shibboleth.idp.authn.AuthnEventIds;
 import net.shibboleth.idp.authn.context.AuthenticationContext;
 import net.shibboleth.idp.authn.context.UsernamePasswordContext;
 import net.shibboleth.idp.authn.principal.UsernamePrincipal;
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullAfterInit;
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
 import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Constraint;
 import net.shibboleth.utilities.java.support.primitive.StringSupport;
 
+import org.ietf.jgss.GSSContext;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.ietf.jgss.GSSManager;
+import org.ietf.jgss.GSSName;
+import org.ietf.jgss.Oid;
 import org.opensaml.profile.action.ActionSupport;
 import org.opensaml.profile.action.EventIds;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import sun.security.jgss.krb5.Krb5Util;
-import sun.security.krb5.Config;
-import sun.security.krb5.Credentials;
-import sun.security.krb5.internal.ktab.KeyTab;
-import sun.security.krb5.EncryptionKey;
-import sun.security.krb5.KrbApReq;
-import sun.security.krb5.KrbAsReqBuilder;
-import sun.security.krb5.KrbException;
-import sun.security.krb5.PrincipalName;
 
 /**
  * An action that checks for a {@link UsernamePasswordContext} and directly produces an
@@ -66,10 +73,13 @@ import sun.security.krb5.PrincipalName;
  * ProfileRequestContext, AuthenticationContext, Exception, String)} method is called.
  */
 public class ValidateUsernamePasswordAgainstKerberos extends AbstractValidationAction {
-
+    
     /** Class logger. */
     @Nonnull private final Logger log = LoggerFactory.getLogger(ValidateUsernamePasswordAgainstKerberos.class);
 
+    /** Class name of JAAS LoginModule to acquire Kerberos credentials. */
+    @NonnullAfterInit @NotEmpty private String loginModuleClassName;
+    
     /** Refresh the Kerberos config before running? */
     private boolean refreshKrb5Config;
     
@@ -81,12 +91,32 @@ public class ValidateUsernamePasswordAgainstKerberos extends AbstractValidationA
     
     /** Path to keytab for service principal. */
     private String keytabPath;
-    
+
+    /** JAAS options for client login. */
+    @NonnullAfterInit private Map<String,String> clientOptions;
+
+    /** JAAS options for server login. */
+    @NonnullAfterInit private Map<String,String> serverOptions;
+
     /** UsernamePasswordContext containing the credentials to validate. */
     @Nullable private UsernamePasswordContext upContext;
     
-    /** Result of authentication. */
-    @Nullable private Credentials krbCreds;
+    /** Constructor. */
+    public ValidateUsernamePasswordAgainstKerberos() {
+        loginModuleClassName = "com.sun.security.auth.module.Krb5LoginModule";
+    }
+    
+    /**
+     * Set the name of the JAAS LoginModule to use to acquire Kerberos credentials.
+     * 
+     * @param name  name of login module class
+     */
+    public void setLoginModuleClassName(@Nonnull final String name) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        loginModuleClassName = Constraint.isNotNull(StringSupport.trimOrNull(name),
+                "Class name cannot be null or empty");
+    }
     
     /**
      * Set whether to refresh the Kerberos configuration before running.
@@ -142,6 +172,23 @@ public class ValidateUsernamePasswordAgainstKerberos extends AbstractValidationA
         if (servicePrincipal != null && keytabPath == null) {
             throw new ComponentInitializationException("A keytab path is required if a service principal is set");
         }
+        
+        clientOptions = new HashMap<>();
+        clientOptions.put("refreshKrb5Config", Boolean.valueOf(refreshKrb5Config).toString());
+
+        if (servicePrincipal != null) {
+            // This set of options is from a lot of trial and error, but a couple of important points:
+            // - setting isInitiator to false prevents an extra AS exchange to get a TGT for the service
+            // - setting storeKey to true is essential or it can't create a GSSCredential for the service
+            serverOptions = new HashMap<>();
+            serverOptions.put("refreshKrb5Config", Boolean.valueOf(refreshKrb5Config).toString());
+            serverOptions.put("useKeyTab", "true");
+            serverOptions.put("keyTab", keytabPath);
+            serverOptions.put("principal", servicePrincipal);
+            serverOptions.put("doNotPrompt", "true");
+            serverOptions.put("isInitiator", "false");
+            serverOptions.put("storeKey", "true");
+        }
     }
     
     /** {@inheritDoc} */
@@ -184,27 +231,34 @@ public class ValidateUsernamePasswordAgainstKerberos extends AbstractValidationA
             @Nonnull final AuthenticationContext authenticationContext) {
         
         try {
-            if (refreshKrb5Config) {
-                Config.refresh();
+            final LoginModule clientLoginModule = (LoginModule) Class.forName(loginModuleClassName).newInstance();
+            clientLoginModule.initialize(getSubject(), new SimpleCallbackHandler(), new HashMap(), clientOptions);
+            if (!clientLoginModule.login() || !clientLoginModule.commit()) {
+                clientLoginModule.abort();
+                throw new LoginException("Login module reported failure");
             }
             
-            // Build principal name to authenticate.
-            final PrincipalName pname = new PrincipalName(upContext.getUsername(), PrincipalName.KRB_NT_PRINCIPAL);
-            final KrbAsReqBuilder reqBuilder = new KrbAsReqBuilder(pname, upContext.getPassword().toCharArray());
-            
-            // Do the exchange.
-            krbCreds = reqBuilder.action().getCreds();
-            reqBuilder.destroy();
+            // We don't call logout, since that would destroy the contents of the Subject.
             
             if (servicePrincipal != null) {
                 verifyKDC();
             }
             
-            log.info("{} Login by '{}' succeeded", getLogPrefix(), pname.getName());
+            log.info("{} Login by '{}' succeeded", getLogPrefix(), upContext.getUsername());
             
             buildAuthenticationResult(profileRequestContext, authenticationContext);
-        } catch (final KrbException | IOException e) {
-            log.warn("{} Login by {} produced exception", getLogPrefix(), upContext.getUsername(), e);
+        } catch (final InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            log.error("{} Unable to instantiate JAAS module for Kerberos", getLogPrefix(), e);
+            handleError(profileRequestContext, authenticationContext, e, AuthnEventIds.AUTHN_EXCEPTION);
+        } catch (final LoginException e) {
+            log.info("{} Login by {} failed", getLogPrefix(), upContext.getUsername(), e);
+            handleError(profileRequestContext, authenticationContext, e, AuthnEventIds.INVALID_CREDENTIALS);
+        } catch(final GSSException e) {
+            log.warn("{} Login by {} failed during GSS context establishment to verify KDC", getLogPrefix(),
+                    upContext.getUsername(), e);
+            handleError(profileRequestContext, authenticationContext, e, AuthnEventIds.INVALID_CREDENTIALS);
+        } catch (final Exception e) {
+            log.warn("{} Login by {} produced unknown exception", getLogPrefix(), upContext.getUsername(), e);
             handleError(profileRequestContext, authenticationContext, e, AuthnEventIds.AUTHN_EXCEPTION);
         }
     }
@@ -212,11 +266,11 @@ public class ValidateUsernamePasswordAgainstKerberos extends AbstractValidationA
     /** {@inheritDoc} */
     @Override
     @Nonnull protected Subject populateSubject(@Nonnull final Subject subject) {
+
         subject.getPrincipals().add(new UsernamePrincipal(upContext.getUsername()));
-        subject.getPrincipals().add(new KerberosPrincipal(krbCreds.getClient().getName()));
         
-        if (preserveTicket) {
-            subject.getPrivateCredentials().add(Krb5Util.credsToTicket(krbCreds));
+        if (!preserveTicket) {
+            subject.getPrivateCredentials().clear();
         }
         
         return subject;
@@ -225,31 +279,108 @@ public class ValidateUsernamePasswordAgainstKerberos extends AbstractValidationA
     /**
      * Use credentials to acquire and verify a service ticket.
      * 
-     * @throws IOException
-     * @throws KrbException
+     * @throws Exception if an error occurs
      */
-    private void verifyKDC() throws KrbException, IOException {
-        log.debug("{} Attempting to verify authenticity of TGT using service principal '{}'", getLogPrefix(),
-                servicePrincipal);
+    private void verifyKDC() throws Exception {
+        log.debug("{} TGT acquired for {}, attempting to verify authenticity of TGT using service principal {}",
+                getLogPrefix(), upContext.getUsername(), servicePrincipal);
         
-        final Credentials serviceCreds = Credentials.acquireServiceCreds(servicePrincipal, krbCreds);
+        final Oid mechOid = new Oid("1.2.840.113554.1.2.2");
         
-        final KeyTab keytab = KeyTab.getInstance(keytabPath);
-        if (!keytab.isValid() || keytab.isMissing()) {
-            throw new IOException("Service principal keytab was missing or invalid, unable to verify KDC");
+        LoginModule serverLoginModule = null;
+        try {
+            serverLoginModule = (LoginModule) Class.forName(loginModuleClassName).newInstance();
+            final Subject serverSubject = new Subject();
+            serverLoginModule.initialize(serverSubject, null, new HashMap(), serverOptions);
+            if (!serverLoginModule.login() || !serverLoginModule.commit()) {
+                serverLoginModule.abort();
+                throw new LoginException("Login module reported failure");
+            }
+            
+            final GSSManager manager = GSSManager.getInstance();
+            
+            // Note the use of NT_USER_NAME; using NT_HOSTBASED_SERVICE does not work and results in a TGS request
+            // to the KDC for an unknown service name that isn't even logged there.
+            final GSSName serviceName = manager.createName(servicePrincipal, GSSName.NT_USER_NAME);
+            final GSSContext context = manager.createContext(serviceName, mechOid, null, GSSContext.DEFAULT_LIFETIME);
+            
+            // The GSS context initiation has to be performed as a privileged action with the client subject
+            // so that the null credential above indicating the default credentials pulls from the JAAS subject.
+            final byte[] token = Subject.doAs(getSubject(), new PrivilegedExceptionAction<byte[]>() {
+                public byte[] run() throws GSSException {
+                    byte[] token = new byte[0];
+                    // This is a one pass context initialization.
+                    context.requestMutualAuth(false);
+                    context.requestCredDeleg(false);
+                    return context.initSecContext(token, 0, token.length);
+                }
+            });
+            
+            // At this point the KDC has logged the additional TGS exchange to get the service ticket.
+            // Because we used the storeKeys option on the server-side JAAS module call, the keytab
+            // entries have been loaded, and the KerberosKey objects in the private credential set are
+            // the credentials needed by GSS to accept the context token, namely to decrypt the service
+            // ticket. So we can use a null GSSCredential again by running the context creation call
+            // under the subject's privilege context.
+            final String verifiedName = Subject.doAs(serverSubject, new PrivilegedExceptionAction<String>() {
+                public String run() throws GSSException {
+                    final GSSContext serverCtx = manager.createContext((GSSCredential) null);
+                    serverCtx.acceptSecContext(token, 0, token.length);
+                    final String s = serverCtx.getSrcName().toString();
+                    serverCtx.dispose();
+                    return s;
+                }
+            });
+            
+            context.dispose();
+            
+            log.debug("{} GSS context established between {} and {}", getLogPrefix(), verifiedName, servicePrincipal);
+        } catch (final LoginException e) {
+            throw new LoginException("Unable to obtain service credentials for KDC verification");
+        } catch (final PrivilegedActionException e) {
+            if (e.getException() != null) {
+                throw e.getException();
+            } else {
+                throw e;
+            }
+        } finally {
+            if (serverLoginModule != null) {
+                serverLoginModule.logout();
+            }
         }
-        final EncryptionKey[] serviceKeys = keytab.readServiceKeys(new PrincipalName(servicePrincipal));
-        if (serviceKeys.length == 0) {
-            throw new KrbException("No service keys found in keytab file, unable to verify KDC");
-        }
-        
-        // TODO: this no longer works on 8, so we're probably going to have to redo all this
-        // on top of GSS-API to make it viable.
-        final KrbApReq request = new KrbApReq(serviceCreds, false, false, false, null);
-        //final KrbApReq decrypted = new KrbApReq(request.getMessage(), serviceKeys, null);
-        
-        log.debug("{} Successfully decrypted AP_REQ issued to service principal '{}'", getLogPrefix(),
-                servicePrincipal);
     }
     
+    /**
+     * A callback handler that provides static name and password data to a JAAS login process.
+     * 
+     * This handler only supports {@link NameCallback} and {@link PasswordCallback}.
+     */
+    private class SimpleCallbackHandler implements CallbackHandler {
+
+        /**
+         * Handle a callback.
+         * 
+         * @param callbacks The list of callbacks to process.
+         * 
+         * @throws UnsupportedCallbackException If callbacks has a callback other than {@link NameCallback} or
+         *             {@link PasswordCallback}.
+         */
+        public void handle(final Callback[] callbacks) throws UnsupportedCallbackException {
+
+            if (callbacks == null || callbacks.length == 0) {
+                return;
+            }
+
+            for (Callback cb : callbacks) {
+                if (cb instanceof NameCallback) {
+                    NameCallback ncb = (NameCallback) cb;
+                    ncb.setName(upContext.getUsername());
+                } else if (cb instanceof PasswordCallback) {
+                    PasswordCallback pcb = (PasswordCallback) cb;
+                    pcb.setPassword(upContext.getPassword().toCharArray());
+                }
+            }
+        }
+    }
+
 }
