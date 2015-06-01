b815bdd97667d231bf5c134578509eb8da2e7d77
Tom Zeller <tzeller@dragonacea.biz>
2015-06-01 20:48:55 +0000

    IDP-594 Some work on the local storage read flow, make the item key a variable.

--- idp-conf/src/main/resources/system/views/local-storage/local-storage-read.js
+++ idp-conf/src/main/resources/system/views/local-storage/local-storage-read.js
@@ -1,15 +1,23 @@
-function readLocalStorageAndSubmit() {
+function readLocalStorageAndSubmit(key) {
     var localStorageSupported = isLocalStorageSupported();
-    document.form1["shib_idp_ls_support"].value = localStorageSupported;
+    document.form1["shib_idp_ls_supported"].value = localStorageSupported;
     if (localStorageSupported) {
-        document.form1["shib_idp_ls"].value = localStorage.getItem("shib_idp_ls");
+        var success;
+        try {
+            document.form1["shib_idp_ls_value"].value = localStorage.getItem(key);
+            success = "true";
+        } catch (e) {
+            success = "false";
+            document.form1["shib_idp_ls_exception"].value = e;
+        }
+        document.form1["shib_idp_ls_success"].value = success;
     }
     document.form1.submit();
 }
 function isLocalStorageSupported() {
     try {
-        localStorage.setItem("testKey", "testValue");
-        localStorage.removeItem("testKey");
+        localStorage.setItem("shib_idp_ls_test", "shib_idp_ls_test");
+        localStorage.removeItem("shib_idp_ls_test");
         return true;
     } catch (e) {
         return false;
