6998022b7e55f6fbecb770866194588476e5ba09
Scott Cantor <cantor.2@osu.edu>
2015-07-30 15:20:29 +0000

    IDP-748 - Rename of idp-consent module to delineate impl classes

--- idp-conf/src/test/java/net/shibboleth/idp/test/flows/cas/LoginFlowTest.java
+++ idp-conf/src/test/java/net/shibboleth/idp/test/flows/cas/LoginFlowTest.java
@@ -25,7 +25,7 @@ import net.shibboleth.idp.authn.context.SubjectContext;
 import net.shibboleth.idp.cas.config.LoginConfiguration;
 import net.shibboleth.idp.cas.ticket.Ticket;
 import net.shibboleth.idp.cas.ticket.TicketService;
-import net.shibboleth.idp.consent.context.ConsentContext;
+import net.shibboleth.idp.consent.context.impl.ConsentContext;
 import net.shibboleth.idp.profile.context.RelyingPartyContext;
 import net.shibboleth.idp.relyingparty.RelyingPartyConfiguration;
 import net.shibboleth.idp.relyingparty.RelyingPartyConfigurationResolver;
