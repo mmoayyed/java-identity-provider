d1d6b92297cc27ecfb5bc9b58f3ef15b44d20f32
Scott Cantor <cantor.2@osu.edu>
2016-03-11 00:31:44 +0000

    Fix warnings.

--- idp-profile-impl/src/main/java/net/shibboleth/idp/profile/impl/PopulateUserAgentContext.java
+++ idp-profile-impl/src/main/java/net/shibboleth/idp/profile/impl/PopulateUserAgentContext.java
@@ -1,9 +1,9 @@
 /*
- * Licensed to the University Corporation for Advanced Internet Development,
- * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
  * NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The UCAID licenses this file to You under the Apache
- * License, Version 2.0 (the "License"); you may not use this file except in
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
  * compliance with the License.  You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
@@ -34,10 +34,12 @@ import org.opensaml.profile.logic.BrowserProfilePredicate;
  */
 public class PopulateUserAgentContext extends AbstractProfileAction {
 
+    /** Constructor. */
     public PopulateUserAgentContext() {
         setActivationCondition(new BrowserProfilePredicate());
     }
 
+    /** {@inheritDoc} */
     @Override
     protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
         final UserAgentContext uac = new UserAgentContext();
