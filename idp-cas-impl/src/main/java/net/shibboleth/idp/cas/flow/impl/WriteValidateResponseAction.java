c5cd0e3fae0474cd06d530ae13bd96132d6b877e
Marvin S. Addison <marvin.addison@gmail.com>
2016-05-05 14:37:46 +0000

    IDP-949 Implement CAS 1.0 protocol endpoint.
    https://issues.shibboleth.net/jira/browse/IDP-949
    This commit provides support for the CAS 1.0 /validate ticket validation
    endpoint that returns a plain-text response.

---
+++ idp-cas-impl/src/main/java/net/shibboleth/idp/cas/flow/impl/WriteValidateResponseAction.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.cas.flow.impl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Nonnull;
+import javax.servlet.http.HttpServletResponse;
+
+import net.shibboleth.idp.cas.protocol.TicketValidationRequest;
+import net.shibboleth.idp.cas.protocol.TicketValidationResponse;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+
+/**
+ * CAS 1.0 protocol response handler.
+ *
+ * @author Marvin S. Addison
+ * @since 3.3.0
+ */
+public class WriteValidateResponseAction extends
+        AbstractCASProtocolAction<TicketValidationRequest, TicketValidationResponse>  {
+    /** CAS 1.0 protocol content type is plain text. */
+    private static final String CONTENT_TYPE = "text/plain;charset=utf-8";
+
+    /** Protocol success flag indicates what kind of response to provide. */
+    private final boolean success;
+
+    public WriteValidateResponseAction(final boolean successFlag) {
+        this.success = successFlag;
+    }
+
+    @Override
+    protected Event doExecute(
+            final @Nonnull RequestContext springRequestContext,
+            final @Nonnull ProfileRequestContext profileRequestContext) {
+
+        final TicketValidationResponse response = getCASResponse(profileRequestContext);
+        try {
+            final HttpServletResponse servletResponse =
+                    (HttpServletResponse) springRequestContext.getExternalContext().getNativeResponse();
+            servletResponse.setContentType(CONTENT_TYPE);
+            final PrintWriter output = servletResponse.getWriter();
+            if (success) {
+                output.print("yes\n");
+                output.print(response.getUserName() + '\n');
+            } else {
+                output.print("no\n\n");
+            }
+            output.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("IO error writing CAS protocol response", e);
+        }
+        return null;
+    }
+}
---
+++ idp-conf/src/test/java/net/shibboleth/idp/test/flows/cas/ValidateFlowTest.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.test.flows.cas;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.idp.attribute.context.AttributeContext;
+import net.shibboleth.idp.cas.ticket.ServiceTicket;
+import net.shibboleth.idp.cas.ticket.TicketServiceEx;
+import net.shibboleth.idp.cas.ticket.TicketState;
+import net.shibboleth.idp.profile.context.RelyingPartyContext;
+import net.shibboleth.idp.session.IdPSession;
+import net.shibboleth.idp.session.SessionManager;
+import net.shibboleth.idp.session.SessionResolver;
+import net.shibboleth.idp.session.criterion.SessionIdCriterion;
+import net.shibboleth.idp.test.flows.AbstractFlowTest;
+import net.shibboleth.utilities.java.support.resolver.CriteriaSet;
+import org.joda.time.DateTime;
+import org.joda.time.Instant;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.webflow.execution.FlowExecutionOutcome;
+import org.springframework.webflow.executor.FlowExecutionResult;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Tests the flow behind the <code>/validate</code> endpoint.
+ *
+ * @author Marvin S. Addison
+ */
+@ContextConfiguration(locations = {
+        "/test/test-cas-beans.xml",
+})
+public class ValidateFlowTest extends AbstractFlowTest {
+
+    /** Flow id. */
+    @Nonnull
+    private static String FLOW_ID = "cas/validate";
+
+    @Autowired
+    private TicketServiceEx ticketService;
+
+    @Autowired
+    private SessionResolver sessionResolver;
+
+    @Autowired
+    private SessionManager sessionManager;
+
+    @Test
+    public void testSuccess() throws Exception {
+        final String principal = "john";
+        final IdPSession session = sessionManager.createSession(principal);
+        final ServiceTicket ticket = ticketService.createServiceTicket(
+                "ST-1415133132-ompog68ygxKyX9BPwPuw0hESQBjuA",
+                DateTime.now().plusSeconds(5).toInstant(),
+                "https://test.example.org/",
+                new TicketState(session.getId(), principal, Instant.now(), "Password"),
+                false);
+
+        externalContext.getMockRequestParameterMap().put("service", ticket.getService());
+        externalContext.getMockRequestParameterMap().put("ticket", ticket.getId());
+        overrideEndStateOutput(FLOW_ID, "ValidateSuccess");
+
+        final FlowExecutionResult result = flowExecutor.launchExecution(FLOW_ID, null, externalContext);
+
+        final String responseBody = response.getContentAsString();
+        final FlowExecutionOutcome outcome = result.getOutcome();
+        assertEquals(outcome.getId(), "ValidateSuccess");
+        assertTrue(responseBody.contentEquals("yes\njohn\n"));
+        assertEquals("text/plain;charset=utf-8", response.getContentType());
+
+        final IdPSession updatedSession = sessionResolver.resolveSingle(
+                new CriteriaSet(new SessionIdCriterion(session.getId())));
+        assertNotNull(updatedSession);
+        assertEquals(updatedSession.getSPSessions().size(), 0);
+    }
+
+    @Test
+    public void testFailureTicketExpired() throws Exception {
+        externalContext.getMockRequestParameterMap().put("service", "https://test.example.org/");
+        externalContext.getMockRequestParameterMap().put("ticket", "ST-123-ABC");
+
+        final FlowExecutionResult result = flowExecutor.launchExecution(FLOW_ID, null, externalContext);
+
+        assertEquals(result.getOutcome().getId(), "ProtocolErrorView");
+        final String responseBody = response.getContentAsString();
+        assertTrue(responseBody.contentEquals("no\n\n"));
+        assertEquals("text/plain;charset=utf-8", response.getContentType());
+    }
+}
