c123bd7df59d2b292badcccabc3f6279d450d0a7
Tom Zeller <tzeller@dragonacea.biz>
2016-07-09 22:04:20 +0000

    Revert r8289 which breaks the test "API"
    Do not rename a public field, just add the new one.

--- idp-conf/src/test/java/net/shibboleth/idp/test/flows/saml2/SAML2TestResponseValidator.java
+++ idp-conf/src/test/java/net/shibboleth/idp/test/flows/saml2/SAML2TestResponseValidator.java
@@ -84,7 +84,7 @@ public class SAML2TestResponseValidator extends SAML2TestStatusResponseTypeValid
     @Nonnull public String subjectConfirmationMethod = SubjectConfirmation.METHOD_BEARER;
     
     /** Expected subject confirmation data address range for IPv4 addresses. Defaults to "127.0.0.1/32". */
-    @Nonnull public IPRange subjectConfirmationDataAddressRangeV4 = IPRange.parseCIDRBlock("127.0.0.1/32");
+    @Nonnull public IPRange subjectConfirmationDataAddressRange = IPRange.parseCIDRBlock("127.0.0.1/32");
     
     /** Expected subject confirmation data address range for IPv6 addresses. Defaults to "::1/128". */
     @Nonnull public IPRange subjectConfirmationDataAddressRangeV6 = IPRange.parseCIDRBlock("::1/128");
@@ -434,7 +434,7 @@ public class SAML2TestResponseValidator extends SAML2TestStatusResponseTypeValid
     public void assertSubjectConfirmationData(@Nullable final SubjectConfirmationData subjectConfirmationData) {
         final InetAddress address = InetAddresses.forString(subjectConfirmationData.getAddress());
         if (address instanceof Inet4Address) {
-            Assert.assertTrue(subjectConfirmationDataAddressRangeV4.contains(address));
+            Assert.assertTrue(subjectConfirmationDataAddressRange.contains(address));
         } else if(address instanceof Inet6Address) {
             Assert.assertTrue(subjectConfirmationDataAddressRangeV6.contains(address));
         } else {
