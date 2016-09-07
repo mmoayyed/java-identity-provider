eb904cec6796f5fc814201355286d19b8c683fa0
Scott Cantor <cantor.2@osu.edu>
2016-09-07 01:29:34 +0000

    IDP-1031 - ExternalAuthentication ignores authnInstant
    https://issues.shibboleth.net/jira/browse/IDP-1031
    
    Add unit test.

---
+++ idp-authn-impl/src/test/java/net/shibboleth/idp/authn/impl/ValidateExternalAuthenticationTest.java
@@ -0,0 +1,147 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.authn.impl;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginException;
+import javax.servlet.http.HttpServletRequest;
+
+import net.shibboleth.idp.authn.AuthnEventIds;
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.authn.context.AuthenticationErrorContext;
+import net.shibboleth.idp.authn.context.ExternalAuthenticationContext;
+import net.shibboleth.idp.authn.principal.TestPrincipal;
+import net.shibboleth.idp.authn.principal.UsernamePrincipal;
+import net.shibboleth.idp.profile.ActionTestingSupport;
+
+import org.joda.time.DateTime;
+import org.opensaml.profile.action.EventIds;
+import org.springframework.webflow.execution.Event;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/** {@link ValidateExternalAuthentication} unit test. */
+public class ValidateExternalAuthenticationTest extends BaseAuthenticationContextTest {
+    
+    private ValidateExternalAuthentication action;
+    
+    @BeforeMethod public void setUp() throws Exception {
+        super.setUp();
+        
+        prc.getSubcontext(AuthenticationContext.class).setAttemptedFlow(authenticationFlows.get(0));
+
+        action = new ValidateExternalAuthentication();
+        action.setHttpServletRequest((HttpServletRequest) src.getExternalContext().getNativeRequest());
+        action.initialize();
+    }
+
+    @Test public void testMissingFlow() {
+        prc.getSubcontext(AuthenticationContext.class).setAttemptedFlow(null);
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, EventIds.INVALID_PROFILE_CTX);
+    }
+    
+    @Test public void testMissingContext() {
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, AuthnEventIds.INVALID_AUTHN_CTX);
+    }
+
+    @Test public void testNoCredentials() {
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        ac.getSubcontext(ExternalAuthenticationContext.class, true);
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, AuthnEventIds.NO_CREDENTIALS);
+    }
+
+    @Test public void testPrincipalName() {
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        final ExternalAuthenticationContext eac = ac.getSubcontext(ExternalAuthenticationContext.class, true);
+        eac.setPrincipalName("foo");
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNotNull(ac.getAuthenticationResult());
+        Assert.assertEquals(ac.getAuthenticationResult().getSubject().getPrincipals(
+                UsernamePrincipal.class).iterator().next().getName(), "foo");
+    }
+    
+    @Test public void testPrincipal() {
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        final ExternalAuthenticationContext eac = ac.getSubcontext(ExternalAuthenticationContext.class, true);
+        eac.setPrincipal(new TestPrincipal("foo"));
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNotNull(ac.getAuthenticationResult());
+        Assert.assertEquals(ac.getAuthenticationResult().getSubject().getPrincipals(
+                TestPrincipal.class).iterator().next().getName(), "foo");
+    }
+
+    @Test public void testSubject() {
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        final ExternalAuthenticationContext eac = ac.getSubcontext(ExternalAuthenticationContext.class, true);
+        final Subject subject = new Subject();
+        eac.setSubject(subject);
+        subject.getPrincipals().add(new TestPrincipal("foo"));
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNotNull(ac.getAuthenticationResult());
+        Assert.assertEquals(ac.getAuthenticationResult().getSubject().getPrincipals(
+                TestPrincipal.class).iterator().next().getName(), "foo");
+    }
+
+    @Test public void testAuthnInstant() {
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        final ExternalAuthenticationContext eac = ac.getSubcontext(ExternalAuthenticationContext.class, true);
+        eac.setPrincipalName("foo");
+        final DateTime ts = DateTime.now().minus(3600);
+        eac.setAuthnInstant(ts);
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNotNull(ac.getAuthenticationResult());
+        Assert.assertEquals(ts.getMillis(), ac.getAuthenticationResult().getAuthenticationInstant());
+    }
+
+    @Test public void testException() {
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        final ExternalAuthenticationContext eac = ac.getSubcontext(ExternalAuthenticationContext.class, true);
+        eac.setAuthnException(new LoginException("foo"));
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, AuthnEventIds.AUTHN_EXCEPTION);
+        Assert.assertNull(ac.getAuthenticationResult());
+        Assert.assertNotNull(ac.getSubcontext(AuthenticationErrorContext.class));
+        Assert.assertEquals(ac.getSubcontext(AuthenticationErrorContext.class).getExceptions().size(), 1);
+    }
+
+    @Test public void testError() {
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        final ExternalAuthenticationContext eac = ac.getSubcontext(ExternalAuthenticationContext.class, true);
+        eac.setAuthnError("foo");
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, AuthnEventIds.AUTHN_EXCEPTION);
+        Assert.assertNull(ac.getAuthenticationResult());
+    }
+
+}
