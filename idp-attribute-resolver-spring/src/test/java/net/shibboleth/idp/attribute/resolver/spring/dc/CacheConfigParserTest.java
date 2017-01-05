96760b58acb22b81c209e03f9490be8ded073e90
Rod Widdowson <rdw@steadingsoftware.com>
2017-01-05 15:04:45 +0000

    IDP-1093 Add frame work to play with parsing ResultCache
    https://issues.shibboleth.net/jira/browse/IDP-1093
    
    There is actually no testing going on (since we cannot reach into the cache we get and look at the parameterization and life is too short to write the code to test TTL), but with this in place it is easy to walk the code to ensure that the new attributes are parsed correctlt.

---
+++ idp-attribute-resolver-spring/src/test/java/net/shibboleth/idp/attribute/resolver/spring/dc/CacheConfigParserTest.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.attribute.resolver.spring.dc;
+
+import java.io.IOException;
+
+import net.shibboleth.idp.attribute.resolver.spring.dc.impl.CacheConfigParser;
+import net.shibboleth.utilities.java.support.xml.ParserPool;
+import net.shibboleth.utilities.java.support.xml.XMLParserException;
+
+import org.opensaml.core.OpenSAMLInitBaseTestCase;
+import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+
+import com.google.common.cache.Cache;
+
+/**
+ * Code to exercise the ResultCache Parser outwith the DataConnector parsers.
+ * There is no actual testing since we cannot look at what is created.
+ */
+public class CacheConfigParserTest extends OpenSAMLInitBaseTestCase {
+    //
+    
+    @Test public void ttl() throws XMLParserException, IOException {
+        final ParserPool parserPool = XMLObjectProviderRegistrySupport.getParserPool();
+        final Resource resource= new ClassPathResource("net/shibboleth/idp/attribute/resolver/spring/dc/ResultCacheElementTimeToLive.xml");
+        final Document doc = parserPool.parse(resource.getInputStream());
+            final CacheConfigParser ccp = new CacheConfigParser(doc.getDocumentElement());
+        
+        final GenericApplicationContext context = new GenericApplicationContext();
+        
+        context.registerBeanDefinition("name1", ccp.createCache());
+        context.refresh();
+        @SuppressWarnings("unused")
+        final Cache foo = context.getBean("name1", Cache.class);
+        
+        context.close();
+    }
+    
+}
