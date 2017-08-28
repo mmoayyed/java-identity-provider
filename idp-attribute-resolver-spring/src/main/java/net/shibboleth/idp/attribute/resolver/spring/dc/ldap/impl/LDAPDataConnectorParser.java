0e5da044198e583e9e28c4f7964f072b33dd7b73
Scott Cantor <cantor.2@osu.edu>
2017-08-28 10:07:50 -0400

    Roll back warning since it fires on non-TLS configs.

--- idp-attribute-resolver-spring/src/main/java/net/shibboleth/idp/attribute/resolver/spring/dc/ldap/impl/LDAPDataConnectorParser.java
+++ idp-attribute-resolver-spring/src/main/java/net/shibboleth/idp/attribute/resolver/spring/dc/ldap/impl/LDAPDataConnectorParser.java
@@ -373,9 +373,6 @@ public class LDAPDataConnectorParser extends AbstractWarningDataConnectorParser 
                 }
                 result.addPropertyValue("trustCredential",
                         SpringSupport.parseCustomElements(trustElements, parserContext).get(0));
-            } else {
-                DeprecationSupport.warn(ObjectType.CONFIGURATION, "Use of default JVM trust store", logPrefix,
-                        "trustFile attribute");
             }
 
             final List<Element> authElements =
