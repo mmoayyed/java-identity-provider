9ca6eb898557f3b6219712f851df3131b1e5b4c0
Scott Cantor <cantor.2@osu.edu>
2018-07-31 19:27:12 -0400

    Override Spring context class to fix failing tests.

--- idp-conf/src/test/java/net/shibboleth/idp/test/spring/IdPPropertiesApplicationContextInitializerTest.java
+++ idp-conf/src/test/java/net/shibboleth/idp/test/spring/IdPPropertiesApplicationContextInitializerTest.java
@@ -41,6 +41,7 @@ public class IdPPropertiesApplicationContextInitializerTest {
 
     @BeforeMethod public void setUp() {
         sc = new MockServletContext("");
+        sc.addInitParameter(ContextLoader.CONTEXT_CLASS_PARAM, "net.shibboleth.ext.spring.context.DelimiterAwareApplicationContext");
         sc.addInitParameter(ContextLoader.CONFIG_LOCATION_PARAM, "classpath:/system/conf/global-system.xml");
         sc.addInitParameter(ContextLoader.CONTEXT_INITIALIZER_CLASSES_PARAM,
                 IdPPropertiesApplicationContextInitializer.class.getName());
