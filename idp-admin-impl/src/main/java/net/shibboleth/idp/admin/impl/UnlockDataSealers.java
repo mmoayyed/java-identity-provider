a6ce06113d03b3e76958d6bf5fbdd57c76fa496f
Scott Cantor <cantor.2@osu.edu>
2018-07-31 17:06:39 -0400

    IDP-1275 - Deferred decryption of private key
    https://issues.shibboleth.net/jira/browse/IDP-1275
    
    Lightly tested webflow with some defaults/examples.

---
+++ idp-admin-impl/src/main/java/net/shibboleth/idp/admin/impl/UnlockDataSealers.java
@@ -0,0 +1,150 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.admin.impl;
+
+import java.security.KeyException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletRequest;
+
+import net.shibboleth.idp.profile.AbstractProfileAction;
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.security.BasicKeystoreKeyStrategy;
+
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
+
+/**
+ * Action that sets keystore and key passwords for one or more DataSealer KeyStrategy
+ * objects based on query parameters.
+ * 
+ * <p>The only type supported is the basic strategy type provided with the software.</p>
+ * 
+ * <p>An error event will be signaled after the first unsuccessful unlock operation.</p>
+ * 
+ * @event {@link EventIds#PROCEED_EVENT_ID}
+ * @event {@link EventIds#INVALID_PROFILE_CTX}
+ * @event {@link EventIds#INVALID_MESSAGE}
+ * 
+ * @since 3.4.0
+ */
+public class UnlockDataSealers extends AbstractProfileAction {
+    
+    /** Name of keystore password parameter. */
+    @Nonnull @NotEmpty public static final String KEYSTORE_PASSWORD_PARAM_NAME = "keystorePassword";
+
+    /** Name of key password parameter. */
+    @Nonnull @NotEmpty public static final String KEY_PASSWORD_PARAM_NAME = "keyPassword";
+
+    /** Class logger. */
+    @Nonnull private Logger log = LoggerFactory.getLogger(UnlockDataSealers.class);
+    
+    /** Key source(s) to operate on. */
+    @Nonnull @NonnullElements private Collection<BasicKeystoreKeyStrategy> keyStrategies;
+
+    /** Constructor. */
+    public UnlockDataSealers() {
+        keyStrategies = Collections.emptyList();
+    }
+    
+    /**
+     * Set the {@link BasicKeystoreKeyStrategy} objects to inject passwords into.
+     * 
+     * @param strategies objects to unlock
+     */
+    public void setKeyStrategies(@Nullable @NonnullElements final Collection<BasicKeystoreKeyStrategy> strategies) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        if (strategies != null && !strategies.isEmpty()) {
+            keyStrategies = new ArrayList<>(Collections2.filter(strategies, Predicates.notNull()));
+        } else {
+            keyStrategies = Collections.emptyList();
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(final ProfileRequestContext profileRequestContext) {
+        
+        if (!super.doPreExecute(profileRequestContext) || keyStrategies.isEmpty()) {
+            return false;
+        } else if (getHttpServletRequest() == null) {
+            log.warn("{} No HttpServletRequest available", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+            return false;
+        }
+
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void doExecute(final ProfileRequestContext profileRequestContext) {
+
+        final HttpServletRequest request = getHttpServletRequest();
+        
+        final String[] keystorePasswords = request.getParameterValues(KEYSTORE_PASSWORD_PARAM_NAME);
+        final String[] keyPasswords = request.getParameterValues(KEY_PASSWORD_PARAM_NAME);
+        
+        if (keystorePasswords == null || keyPasswords == null || keystorePasswords.length != keyStrategies.size()
+                || keyPasswords.length != keyStrategies.size()) {
+            log.warn("{} Password parameter count does not match size of configured KeyStrategy inputs",
+                    getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+            return;
+        }
+        
+        int i = 0;
+        final Iterator<BasicKeystoreKeyStrategy> iter = keyStrategies.iterator();
+        while (iter.hasNext()) {
+            final BasicKeystoreKeyStrategy ks = iter.next();
+            
+            if (keystorePasswords[i] == null || keyPasswords[i] == null) {
+                log.warn("{} Empty password supplied at index {}", getLogPrefix(), i);
+                ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+                return;
+            }
+            
+            ks.setKeystorePassword(keystorePasswords[i]);
+            ks.setKeyPassword(keyPasswords[i]);
+            
+            try {
+                ks.getDefaultKey();
+            } catch (final KeyException e) {
+                log.warn("{} Failed to unlock key strategy in collection with index {}", getLogPrefix(), i);
+                ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+                return;
+            }
+            
+            i++;
+        }
+    }
+    
+}
---
+++ idp-admin-impl/src/main/java/net/shibboleth/idp/admin/impl/UnlockPrivateKeys.java
@@ -0,0 +1,176 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.admin.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.shibboleth.idp.profile.AbstractProfileAction;
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+
+import org.cryptacular.EncodingException;
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.security.credential.MutableCredential;
+import org.opensaml.security.crypto.KeySupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
+
+/**
+ * Action that creates private key objects and injects them into existing
+ * {@link MutableCredential} objects.
+ * 
+ * <p>An error event will be signaled after the first unsuccessful unlock operation.</p>
+ * 
+ * @event {@link EventIds#PROCEED_EVENT_ID}
+ * @event {@link EventIds#INVALID_PROFILE_CTX}
+ * @event {@link EventIds#INVALID_MESSAGE}
+ * 
+ * @since 3.4.0
+ */
+public class UnlockPrivateKeys extends AbstractProfileAction {
+    
+    /** Name of private key password parameter. */
+    @Nonnull @NotEmpty public static final String KEY_PASSWORD_PARAM_NAME = "privateKeyPassword";
+
+    /** Class logger. */
+    @Nonnull private Logger log = LoggerFactory.getLogger(UnlockPrivateKeys.class);
+    
+    /** Credentials to operate on. */
+    @Nonnull @NonnullElements private Collection<MutableCredential> credentials;
+
+    /** Keys to load. */
+    @Nonnull @NonnullElements private Collection<Resource> keyResources;
+
+    /** Constructor. */
+    public UnlockPrivateKeys() {
+        credentials = Collections.emptyList();
+        keyResources = Collections.emptyList();
+    }
+    
+    /**
+     * Set the credentials to load keys into.
+     * 
+     * @param creds credentials to load keys into
+     */
+    public void setCredentials(@Nullable @NonnullElements final Collection<MutableCredential> creds) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        if (creds != null && !creds.isEmpty()) {
+            credentials = new ArrayList<>(Collections2.filter(creds, Predicates.notNull()));
+        } else {
+            credentials = Collections.emptyList();
+        }
+    }
+
+    /**
+     * Set the key resources to load.
+     * 
+     * @param keys key resources to load
+     */
+    public void setKeyResources(@Nullable @NonnullElements final Collection<Resource> keys) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        if (keys != null && !keys.isEmpty()) {
+            keyResources = new ArrayList<>(Collections2.filter(keys, Predicates.notNull()));
+        } else {
+            keyResources = Collections.emptyList();
+        }
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void doInitialize() throws ComponentInitializationException {
+        super.doInitialize();
+        
+        if (keyResources.size() != credentials.size()) {
+            throw new ComponentInitializationException("Size of credential and key resource collections don't match.");
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(final ProfileRequestContext profileRequestContext) {
+        
+        if (!super.doPreExecute(profileRequestContext) || credentials.isEmpty() || keyResources.isEmpty()) {
+            return false;
+        } else if (getHttpServletRequest() == null) {
+            log.warn("{} No HttpServletRequest available", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+            return false;
+        }
+        
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected void doExecute(final ProfileRequestContext profileRequestContext) {
+
+        final String[] keyPasswords = getHttpServletRequest().getParameterValues(KEY_PASSWORD_PARAM_NAME);
+        
+        if (keyPasswords == null || keyPasswords.length != credentials.size()) {
+            log.warn("{} Password parameter count does not match number of configured credentials", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+            return;
+        }
+        
+        int i = 0;
+        Iterator<MutableCredential> c_iter = credentials.iterator();
+        Iterator<Resource> k_iter = keyResources.iterator();
+        while (c_iter.hasNext() && k_iter.hasNext()) {
+            final MutableCredential cred = c_iter.next();
+            final Resource resource = k_iter.next();
+            
+            if (keyPasswords[i] == null) {
+                log.warn("{} Empty password supplied at index {}", getLogPrefix(), i);
+                ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+                return;
+            }
+
+            log.info("{} Attempting unlock of private key in {}", getLogPrefix(), resource.getDescription());
+            
+            try (final InputStream is = resource.getInputStream()) {
+                cred.setPrivateKey(KeySupport.decodePrivateKey(is, keyPasswords[i].toCharArray()));
+                log.info("{} Unlocked and injected private key in {}", getLogPrefix(), resource.getDescription());
+            } catch (final KeyException | IOException | EncodingException e) {
+                log.warn("{} Failed to unlock private key with index {}", getLogPrefix(), i, e);
+                ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+                return;
+            }
+            
+            i++;
+        }
+    }
+    
+}
