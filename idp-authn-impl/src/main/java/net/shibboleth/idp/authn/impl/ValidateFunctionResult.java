d9dd8a5e1e8e5979c048e038a4be955d46877712
Scott Cantor <cantor.2@osu.edu>
2018-09-05 17:30:14 -0400

    IDP-1124 - Function-driven login flow
    https://issues.shibboleth.net/jira/browse/IDP-1124

---
+++ idp-authn-impl/src/main/java/net/shibboleth/idp/authn/impl/ValidateFunctionResult.java
@@ -0,0 +1,170 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.authn.impl;
+
+import java.security.Principal;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.security.auth.Subject;
+
+import net.shibboleth.idp.authn.AbstractValidationAction;
+import net.shibboleth.idp.authn.AuthnEventIds;
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.authn.principal.UsernamePrincipal;
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullAfterInit;
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+import net.shibboleth.utilities.java.support.logic.ConstraintViolationException;
+
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+
+/**
+ * An action that executes a deployer-supplied function and produces an
+ * {@link net.shibboleth.idp.authn.AuthenticationResult} based on the function result.
+ * 
+ * @event {@link EventIds#PROCEED_EVENT_ID}
+ * @event {@link EventIds#INVALID_PROFILE_CTX}
+ * @event {@link AuthnEventIds#INVALID_CREDENTIALS}
+ * @event {@link AuthnEventIds#NO_CREDENTIALS}
+ * @pre <pre>ProfileRequestContext.getSubcontext(AuthenticationContext.class).getAttemptedFlow() != null</pre>
+ * @post If the function returns a String, Principal, or Subject, an
+ * {@link net.shibboleth.idp.authn.AuthenticationResult} is saved to the {@link AuthenticationContext}.
+ * 
+ * @since 3.4.0
+ */
+public class ValidateFunctionResult extends AbstractValidationAction {
+
+    /** Default prefix for metrics. */
+    @Nonnull @NotEmpty private static final String DEFAULT_METRIC_NAME = "net.shibboleth.idp.authn.function";
+    
+    /** Class logger. */
+    @Nonnull private final Logger log = LoggerFactory.getLogger(ValidateFunctionResult.class);
+    
+    /** Function to evaluate. */
+    @NonnullAfterInit private Function<ProfileRequestContext,?> resultLookupStrategy;
+    
+    /** Authentication result. */
+    @Nullable private Object result;
+    
+    /** Constructor. */
+    public ValidateFunctionResult() {
+        setMetricName(DEFAULT_METRIC_NAME);
+    }
+    
+    /**
+     * Set the function to execute to produce the authentication result.
+     * 
+     * <p>The function can return a {@link String}, a {@link Principal}, or a {@link Subject}.</p>
+     * 
+     * @param strategy result strategy
+     */
+    public void setResultLookupStrategy(@Nonnull final Function<ProfileRequestContext,?> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        resultLookupStrategy = Constraint.isNotNull(strategy, "Result lookup strategy cannot be null");
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void doInitialize() throws ComponentInitializationException {
+        super.doInitialize();
+        
+        if (resultLookupStrategy == null) {
+            throw new ComponentInitializationException("Result lookup strategy cannot be null");
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext,
+            @Nonnull final AuthenticationContext authenticationContext) {
+
+        if (!super.doPreExecute(profileRequestContext, authenticationContext)) {
+            return false;
+        }
+        
+        if (authenticationContext.getAttemptedFlow() == null) {
+            log.debug("{} No attempted flow within authentication context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+            recordFailure();
+            return false;
+        }
+        
+        return true;
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext,
+            @Nonnull final AuthenticationContext authenticationContext) {
+
+        result = resultLookupStrategy.apply(profileRequestContext);
+
+        if (result == null) {
+            log.info("{} Authentication by function failed", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, AuthnEventIds.NO_CREDENTIALS);
+            recordFailure();
+        } else if (result instanceof String) {
+            log.info("{} Validated user via name '{}'", getLogPrefix(), result);
+            recordSuccess();
+            buildAuthenticationResult(profileRequestContext, authenticationContext);
+        } else if (result instanceof Principal) {
+            log.info("{} Validated user via Principal '{}'", getLogPrefix(), result);
+            recordSuccess();
+            buildAuthenticationResult(profileRequestContext, authenticationContext);
+        } else if (result instanceof Subject) {
+            log.info("{} Validated user via Subject", getLogPrefix());
+            recordSuccess();
+            buildAuthenticationResult(profileRequestContext, authenticationContext);
+        } else {
+            log.info("{} Authentication by function failed, result type was invalid", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, AuthnEventIds.INVALID_CREDENTIALS);
+            recordFailure();
+        }
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    @Nonnull protected Subject populateSubject(@Nonnull final Subject subject) {
+        
+        if (result instanceof String) {
+            subject.getPrincipals().add(new UsernamePrincipal((String) result));
+            return subject;
+        } else if (result instanceof Principal) {
+            subject.getPrincipals().add((Principal) result);
+            return subject;
+        } else if (result instanceof Subject) {
+            // Override supplied Subject with our own, after transferring over any custom Principals.
+            ((Subject) result).getPrincipals().addAll(subject.getPrincipals());
+            return (Subject) result;
+        }
+        
+        // Save my walrus!
+        throw new ConstraintViolationException("Result type was unexpected");
+    }
+    
+}
---
+++ idp-authn-impl/src/test/java/net/shibboleth/idp/authn/impl/ValidateFunctionResultTest.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.authn.impl;
+
+import java.security.Principal;
+
+import javax.security.auth.Subject;
+
+import net.shibboleth.idp.authn.AuthnEventIds;
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.authn.principal.TestPrincipal;
+import net.shibboleth.idp.authn.principal.UsernamePrincipal;
+import net.shibboleth.idp.profile.ActionTestingSupport;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.logic.FunctionSupport;
+
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.springframework.webflow.execution.Event;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.google.common.base.Functions;
+
+/** {@link ValidateFunctionResult} unit test. */
+public class ValidateFunctionResultTest extends BaseAuthenticationContextTest {
+    
+    private ValidateFunctionResult action;
+    
+    @BeforeMethod public void setUp() throws Exception {
+        super.setUp();
+        
+        prc.getSubcontext(AuthenticationContext.class).setAttemptedFlow(authenticationFlows.get(0));
+
+        action = new ValidateFunctionResult();
+    }
+
+    @Test public void testMissingFlow() throws ComponentInitializationException {
+        prc.getSubcontext(AuthenticationContext.class).setAttemptedFlow(null);
+        
+        action.setResultLookupStrategy(FunctionSupport.<ProfileRequestContext,Object>constant(null));
+        action.initialize();
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, EventIds.INVALID_PROFILE_CTX);
+    }
+    
+    @Test public void testNoCredentials() throws ComponentInitializationException {
+        
+        action.setResultLookupStrategy(FunctionSupport.<ProfileRequestContext,Object>constant(null));
+        action.initialize();
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, AuthnEventIds.NO_CREDENTIALS);
+    }
+
+    @Test public void testInvalidType() throws ComponentInitializationException {
+        
+        action.setResultLookupStrategy(Functions.<ProfileRequestContext>identity());
+        action.initialize();
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertEvent(event, AuthnEventIds.INVALID_CREDENTIALS);
+    }
+
+    @Test public void testPrincipalName() throws ComponentInitializationException {
+        action.setResultLookupStrategy(FunctionSupport.<ProfileRequestContext,String>constant("foo"));
+        action.initialize();
+        
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertProceedEvent(event);
+        
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        Assert.assertNotNull(ac.getAuthenticationResult());
+        Assert.assertFalse(ac.getAuthenticationResult().isPreviousResult());
+        Assert.assertEquals(ac.getAuthenticationResult().getSubject().getPrincipals(
+                UsernamePrincipal.class).iterator().next().getName(), "foo");
+    }
+    
+    @Test public void testPrincipal() throws ComponentInitializationException {
+        action.setResultLookupStrategy(FunctionSupport.<ProfileRequestContext,Principal>constant(new TestPrincipal("foo")));
+        action.initialize();
+
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertProceedEvent(event);
+
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        Assert.assertNotNull(ac.getAuthenticationResult());
+        Assert.assertFalse(ac.getAuthenticationResult().isPreviousResult());
+        Assert.assertEquals(ac.getAuthenticationResult().getSubject().getPrincipals(
+                TestPrincipal.class).iterator().next().getName(), "foo");
+    }
+
+    @Test public void testSubject() throws ComponentInitializationException {
+        final Subject subject = new Subject();
+        subject.getPrincipals().add(new TestPrincipal("foo"));
+
+        action.setResultLookupStrategy(FunctionSupport.<ProfileRequestContext,Subject>constant(subject));
+        action.initialize();
+
+        final Event event = action.execute(src);
+        ActionTestingSupport.assertProceedEvent(event);
+
+        final AuthenticationContext ac = prc.getSubcontext(AuthenticationContext.class);
+        Assert.assertNotNull(ac.getAuthenticationResult());
+        Assert.assertFalse(ac.getAuthenticationResult().isPreviousResult());
+        Assert.assertEquals(ac.getAuthenticationResult().getSubject().getPrincipals(
+                TestPrincipal.class).iterator().next().getName(), "foo");
+    }
+
+}
