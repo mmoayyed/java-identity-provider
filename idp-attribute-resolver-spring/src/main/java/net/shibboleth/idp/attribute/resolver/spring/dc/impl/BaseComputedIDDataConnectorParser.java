fcb2a84355e754d0bc70037c1a639d5efa24934f
Rod Widdowson <rdw@steadingsoftware.com>
2019-01-27 15:01:05 +0000

    IDP-1121 Replace spurious check and warning
    https://issues.shibboleth.net/jira/browse/IDP-1121
    
    There is only one namespace now so we don't need to check against it.

--- idp-attribute-resolver-spring/src/main/java/net/shibboleth/idp/attribute/resolver/spring/dc/impl/BaseComputedIDDataConnectorParser.java
+++ idp-attribute-resolver-spring/src/main/java/net/shibboleth/idp/attribute/resolver/spring/dc/impl/BaseComputedIDDataConnectorParser.java
@@ -31,13 +31,9 @@ import org.w3c.dom.Element;
 
 import net.shibboleth.idp.attribute.resolver.spring.BaseResolverPluginParser;
 import net.shibboleth.idp.attribute.resolver.spring.dc.AbstractDataConnectorParser;
-import net.shibboleth.idp.attribute.resolver.spring.impl.AttributeResolverNamespaceHandler;
 import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
-import net.shibboleth.utilities.java.support.primitive.DeprecationSupport;
-import net.shibboleth.utilities.java.support.primitive.DeprecationSupport.ObjectType;
 import net.shibboleth.utilities.java.support.primitive.StringSupport;
 import net.shibboleth.utilities.java.support.xml.AttributeSupport;
-import net.shibboleth.utilities.java.support.xml.DOMTypeSupport;
 import net.shibboleth.utilities.java.support.xml.ElementSupport;
 
 /**
@@ -63,12 +59,6 @@ public abstract class BaseComputedIDDataConnectorParser extends BaseResolverPlug
     protected void doParse(@Nonnull final Element config, @Nonnull final ParserContext parserContext,
             @Nonnull final BeanDefinitionBuilder builder, @Nullable final String generatedIdDefaultName) {
         super.doParse(config, parserContext, builder);
-        
-        final QName suppliedQname = DOMTypeSupport.getXSIType(config);
-        if (!AttributeResolverNamespaceHandler.NAMESPACE.equals(suppliedQname.getNamespaceURI())) {
-            DeprecationSupport.warnOnce(ObjectType.XSITYPE, suppliedQname.toString(),
-                    parserContext.getReaderContext().getResource().getDescription(), getPreferredName().toString());
-        } 
         
         final String generatedAttribute;
         if (config.hasAttributeNS(null, "generatedAttributeID")) {
