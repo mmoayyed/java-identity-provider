c41b441a34a23606759fbeadb767fa2c0483e95a
Rod Widdowson <rdw@steadingsoftware.com>
2019-06-08 15:37:48 +0100

    IDP-1464 Introduced a processed UIInfo class
    https://issues.shibboleth.net/jira/browse/IDP-1464
    
    This converts lists of language bearing OpenSAML objects
    into Maps<Locale, String>. We filter (and warn) against
    elements with no language except for the logo where we provide the
    Map for ones with languages and an array for the ones without.

---
+++ idp-saml-api/src/test/java/net/shibboleth/idp/saml/metadata/IdPUIInfoTest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.saml.metadata;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+import java.util.Locale;
+
+import org.opensaml.core.xml.XMLObjectBaseTestCase;
+import org.opensaml.saml.ext.saml2mdui.UIInfo;
+import org.testng.annotations.Test;
+
+/**
+ * test for the {@link IdPUIInfo}.
+ */
+public class IdPUIInfoTest extends XMLObjectBaseTestCase {
+    
+    @Test public void test() {
+    
+        final UIInfo samluiinfo = unmarshallElement("/UIInfo.xml");
+        final IdPUIInfo uiInfo = new IdPUIInfo(samluiinfo);
+        
+        assertEquals(uiInfo.getDisplayNames().size(), 2);
+        assertNotNull(uiInfo.getDisplayNames().get(Locale.forLanguageTag("en-us")));
+        assertEquals(uiInfo.getDescriptions().size(), 1);
+        assertEquals(uiInfo.getKeywords().size(), 1);
+        assertEquals(uiInfo.getLocaleLogos().size(), 1);
+        assertEquals(uiInfo.getLocaleLogos().get(Locale.forLanguageTag("en")).size(), 2);
+        assertEquals(uiInfo.getNonLocaleLogos().size(), 1);
+        assertEquals(uiInfo.getInformationURLs().size(), 1);
+        assertEquals(uiInfo.getPrivacyStatementURLs().size(), 1);
+        
+    }
+}
