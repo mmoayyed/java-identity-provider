2534f5517e75b09895a4eae100bcc3376d8059f1
Rod Widdowson <rdw@steadingsoftware.com>
2019-06-10 14:44:58 +0100

    IDP-1464 Introduce a processed UI class for ACS
    https://issues.shibboleth.net/jira/browse/IDP-1464
    
    This means making the ServiceName and ServiceDescription
    available as a Map<Locale,String>

---
+++ idp-saml-api/src/main/java/net/shibboleth/idp/saml/metadata/ACSUIInfo.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.saml.metadata;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
+
+import org.opensaml.saml.saml2.metadata.AttributeConsumingService;
+import org.opensaml.saml.saml2.metadata.LocalizedName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.shibboleth.utilities.java.support.annotation.constraint.Unmodifiable;
+
+/**
+ * Class to contain a processed form of the {@link AttributeConsumingService} suitable for display purposes. */
+public class ACSUIInfo {
+    
+    /** logger. */
+    private static final Logger LOG = LoggerFactory.getLogger(ACSUIInfo.class);
+    
+    /** The Service Names as a map from locale to actual value.*/ 
+    @Nonnull @Unmodifiable private final Map<Locale, String> serviceNames;
+    
+    /** The Service Descriptions as a map from locale to actual value.*/ 
+    @Nonnull @Unmodifiable private final Map<Locale, String> serviceDescriptions;
+       
+    /** Warning check against a non localized String. */
+    private final Predicate<LocalizedName> nullLanguageString = new Predicate<>() {
+        public boolean test(final LocalizedName u) {
+            if (u.getXMLLang() == null) {
+                LOG.warn("String with value {} has no language associated, ignoring", u.getValue());
+                return false;
+            }
+            return true;
+        }
+    };
+
+    /**
+     * Constructor.
+     *
+     * @param acs The OpenSaml AssertionConsumingService to convert.
+     */
+    public ACSUIInfo(@Nonnull final AttributeConsumingService acs) {
+        
+        serviceNames = acs.getNames().
+                stream().
+                filter(nullLanguageString).
+                collect(Collectors.toUnmodifiableMap(
+                        serviceName -> Locale.forLanguageTag(serviceName.getXMLLang()), 
+                        serviceName -> serviceName.getValue()));
+        serviceDescriptions = acs.
+                getDescriptions().
+                stream().
+                filter(nullLanguageString).
+                collect(Collectors.toUnmodifiableMap(
+                        description -> Locale.forLanguageTag(description.getXMLLang()), 
+                        description -> description.getValue()));
+    }
+
+    /** 
+     * Get the Display Names as a map from locale to actual value.
+     * 
+     * @return the display names
+     */
+    @Nonnull @Unmodifiable public Map<Locale, String> getServiceNames() {
+        return serviceNames; 
+    }
+    
+    /**
+     * Return the descriptions as a map from locale to actual value.
+     *
+     * @return the descriptions names (if any)
+     */
+    @Nonnull @Unmodifiable public Map<Locale, String> getServiceDescriptions() {
+        return serviceDescriptions;
+    }
+    
+}
---
+++ idp-saml-api/src/test/java/net/shibboleth/idp/saml/metadata/ACSUIInfoTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.saml.metadata;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Locale;
+
+import org.opensaml.core.xml.XMLObjectBaseTestCase;
+import org.opensaml.core.xml.io.UnmarshallingException;
+import org.opensaml.saml.saml2.metadata.AttributeConsumingService;
+import org.testng.annotations.Test;
+
+import net.shibboleth.utilities.java.support.xml.XMLParserException;
+
+/**
+ * test for the {@link ACSUIInfo}.
+ */
+public class ACSUIInfoTest extends XMLObjectBaseTestCase {
+    
+    @Test public void test() throws XMLParserException, UnmarshallingException {
+    
+        final AttributeConsumingService acs = unmarshallElement("/ACSUIInfo.xml", true);
+        final ACSUIInfo info = new ACSUIInfo(acs); 
+        
+        assertEquals(info.getServiceNames().size(), 2);
+        assertEquals(info.getServiceNames().get(Locale.forLanguageTag("en")), "ServiceName");
+        assertEquals(info.getServiceDescriptions().size(), 1);
+        
+    }
+}
