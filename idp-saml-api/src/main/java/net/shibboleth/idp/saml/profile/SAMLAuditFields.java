5e8dedc629e9979e1dd74fdbd4f479f1a302127b
Scott Cantor <cantor.2@osu.edu>
2019-08-15 13:15:27 -0400

    Add inbound message signing audit field. Add the signed/encrypted fields to default log.

--- idp-saml-api/src/main/java/net/shibboleth/idp/saml/profile/SAMLAuditFields.java
+++ idp-saml-api/src/main/java/net/shibboleth/idp/saml/profile/SAMLAuditFields.java
@@ -92,6 +92,9 @@ public final class SAMLAuditFields {
     /** ForceAuthn requested field. */
     @Nonnull @NotEmpty public static final String FORCE_AUTHN = "fauth";
 
+    /** Signed inbound message field. {@since 4.0.0} */
+    @Nonnull @NotEmpty public static final String SIGNING = "XX";
+
     /** Encryption field. */
     @Nonnull @NotEmpty public static final String ENCRYPTION = "X";
 
