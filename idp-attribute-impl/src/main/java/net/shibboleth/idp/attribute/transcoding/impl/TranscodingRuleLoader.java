0875419aa2972683a3a0479bf749d49976e81129
Scott Cantor <cantor.2@osu.edu>
2020-02-27 11:41:20 -0500

    IDP-1532 - Potentially spurious warning from the transcoding registry
    https://issues.shibboleth.net/jira/browse/IDP-1532
    
    Enhanced with a suffix list, and renamed the README file.

--- idp-attribute-impl/src/main/java/net/shibboleth/idp/attribute/transcoding/impl/TranscodingRuleLoader.java
+++ idp-attribute-impl/src/main/java/net/shibboleth/idp/attribute/transcoding/impl/TranscodingRuleLoader.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +38,7 @@ import net.shibboleth.idp.attribute.transcoding.TranscodingRule;
 import net.shibboleth.utilities.java.support.annotation.ParameterName;
 import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
 import net.shibboleth.utilities.java.support.logic.Constraint;
+import net.shibboleth.utilities.java.support.logic.PredicateSupport;
 
 /**
  * A mechanism for loading a set of {@link TranscodingRule} objects from sources such as maps
@@ -57,12 +59,17 @@ public class TranscodingRuleLoader {
      * 
      * <p>Individual rules that fail to load will be skipped.</p>
      * 
+     * <p>The file extensions must include the period, and only apply to
+     * files, while all directories will be examined.</p>
+     * 
      * @param dir root to search
+     * @param extensions file extensions to include
      * 
      * @throws IOException if an error occurs
      */
-    public TranscodingRuleLoader(@Nonnull @ParameterName(name="dir") final Path dir) throws IOException {
-
+    public TranscodingRuleLoader(@Nonnull @ParameterName(name="dir") final Path dir,
+            @Nullable @NonnullElements @ParameterName(name="extensions") final Collection<String> extensions)
+                    throws IOException {
         log.debug("Loading rules from directory ({})", dir);
         rules = new ArrayList<>();
         
@@ -71,11 +78,12 @@ public class TranscodingRuleLoader {
                 final File file =  child.toFile();
                 if (file.isDirectory()) {
                     try {
-                        rules.addAll(new TranscodingRuleLoader(child).getRules());
+                        rules.addAll(new TranscodingRuleLoader(child, extensions).getRules());
                     } catch (final IOException e) {
                         log.error("Failed to load rules from directory ({})", file, e);
                     }
-                } else {
+                } else if (!extensions.isEmpty() &&
+                        PredicateSupport.anyMatch((String ext) -> file.getName().endsWith(ext)).test(extensions)) {
                     log.debug("Loading rule from property set in file ({})", file);
                     try {
                         final TranscodingRule rule = TranscodingRule.fromResource(new FileSystemResource(file));
@@ -87,9 +95,26 @@ public class TranscodingRuleLoader {
                     } catch (final IOException e) {
                         log.error("Failed to load rule from file ({})", file, e);
                     }
+                } else {
+                    log.debug("Ignoring file ({}) with non-matching extension", file);
                 }
             }
         }
+    }
+
+    /**
+     * Load rules from all files found below a directory root.
+     * 
+     * <p>Files are assumed to be Java property files in text format.</p>
+     * 
+     * <p>Individual rules that fail to load will be skipped.</p>
+     * 
+     * @param dir root to search
+     * 
+     * @throws IOException if an error occurs
+     */
+    public TranscodingRuleLoader(@Nonnull @ParameterName(name="dir") final Path dir) throws IOException {
+        this(dir, null);
     }
     
     /**
