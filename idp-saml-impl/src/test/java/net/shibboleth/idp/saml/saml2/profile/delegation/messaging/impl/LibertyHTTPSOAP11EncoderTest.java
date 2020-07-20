95c12c13b3d1fc2ee7e377d196a2de30819109a1
Scott Cantor <cantor.2@osu.edu>
2020-07-20 08:52:35 -0400

    Make test immune to Java hash changes.

--- idp-saml-impl/src/test/java/net/shibboleth/idp/saml/saml2/profile/delegation/messaging/impl/LibertyHTTPSOAP11EncoderTest.java
+++ idp-saml-impl/src/test/java/net/shibboleth/idp/saml/saml2/profile/delegation/messaging/impl/LibertyHTTPSOAP11EncoderTest.java
@@ -18,11 +18,13 @@
 package net.shibboleth.idp.saml.saml2.profile.delegation.messaging.impl;
 
 import net.shibboleth.idp.saml.saml2.profile.delegation.impl.LibertyConstants;
-import net.shibboleth.utilities.java.support.testing.TestSupport;
 
+import java.io.ByteArrayInputStream;
 import java.time.Instant;
 
+import org.opensaml.core.xml.XMLObject;
 import org.opensaml.core.xml.XMLObjectBaseTestCase;
+import org.opensaml.core.xml.util.XMLObjectSupport;
 import org.opensaml.messaging.context.MessageContext;
 import org.opensaml.saml.common.SAMLObjectBuilder;
 import org.opensaml.saml.common.SAMLVersion;
@@ -35,6 +37,7 @@ import org.opensaml.saml.saml2.core.StatusCode;
 import org.opensaml.saml.saml2.metadata.AssertionConsumerService;
 import org.opensaml.saml.saml2.metadata.Endpoint;
 import org.opensaml.soap.messaging.SOAPMessagingSupport;
+import org.opensaml.soap.soap11.Envelope;
 import org.opensaml.soap.wsaddressing.Action;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.testng.Assert;
@@ -105,10 +108,23 @@ public class LibertyHTTPSOAP11EncoderTest extends XMLObjectBaseTestCase {
         Assert.assertEquals("UTF-8", response.getCharacterEncoding(), "Unexpected character encoding");
         Assert.assertEquals(response.getHeader("Cache-control"), "no-cache, no-store", "Unexpected cache controls");
         Assert.assertEquals(response.getHeader("SOAPAction"), LibertyConstants.SSOS_RESPONSE_WSA_ACTION_URI);
-        // TODO: this hashes differently with endorsed Xerces or under Java 9
-        if (TestSupport.isJavaV9OrLater()) {
-            return;
+
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(response.getContentAsByteArray())) {
+            XMLObject xmlObject = XMLObjectSupport.unmarshallFromInputStream(parserPool, inputStream);
+            Assert.assertNotNull(xmlObject);
+            Assert.assertTrue(xmlObject instanceof Envelope);
+            Envelope envelope = (Envelope) xmlObject;
+            Assert.assertNotNull(envelope.getHeader());
+            Assert.assertEquals(envelope.getHeader().getUnknownXMLObjects().size(), 1);
+            Action outboundAction = (Action) envelope.getHeader().getUnknownXMLObjects().get(0);
+            Assert.assertNotNull(outboundAction);
+            Assert.assertNotNull(envelope.getBody());
+            Assert.assertEquals(envelope.getBody().getUnknownXMLObjects().size(), 1);
+            Response outboundResponse = (Response) envelope.getBody().getUnknownXMLObjects().get(0);
+            outboundResponse.releaseDOM();
+            outboundResponse.releaseChildrenDOM(true);
+            outboundResponse.setParent(null);
+            assertXMLEquals(XMLObjectSupport.marshall(outboundResponse).getOwnerDocument(), samlMessage);
         }
-        Assert.assertEquals(response.getContentAsString().hashCode(), 1113901725);
     }
 }
