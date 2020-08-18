15aba3595cb41326fbce49e1c2648082334b79a9
Scott Cantor <cantor.2@osu.edu>
2020-08-18 18:09:14 -0400

    IDP-1652 - Redesign handling of lists of descriptors
    https://issues.shibboleth.net/jira/browse/IDP-1652
    
    Support auto-wired
    ProfileInterceptorFlowDescriptors.

---
+++ idp-profile-impl/src/main/java/net/shibboleth/idp/profile/interceptor/impl/ProfileInterceptorFlowDescriptorManager.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.profile.interceptor.impl;
+
+import java.util.Collection;
+
+import javax.annotation.Nullable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import net.shibboleth.ext.spring.util.IdentifiedComponentManager;
+import net.shibboleth.idp.profile.interceptor.ProfileInterceptorFlowDescriptor;
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
+
+/**
+ * Manager of {@link ProfileInterceptorFlowDescriptor} objects. 
+ */
+public class ProfileInterceptorFlowDescriptorManager
+        extends IdentifiedComponentManager<ProfileInterceptorFlowDescriptor> {
+
+    /**
+     * Constructor.
+     *
+     * @param freeObjects free-standing objects to add
+     */
+    @Autowired
+    public ProfileInterceptorFlowDescriptorManager(
+            @Nullable @NonnullElements final Collection<ProfileInterceptorFlowDescriptor> freeObjects) {
+        super(freeObjects);
+    }
+
+}
