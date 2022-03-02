ad5252fcfadb77850074f8e2ace625708717682d
Scott Cantor <cantor.2@osu.edu>
2022-03-02 16:00:07 -0500

    IDP-1903 - Remove logback.xml from idp-core
    https://shibboleth.atlassian.net/browse/IDP-1903
    
    Adjust fallback logging config to an internal resource.

--- idp-admin-api/src/main/java/net/shibboleth/idp/log/LogbackLoggingService.java
+++ idp-admin-api/src/main/java/net/shibboleth/idp/log/LogbackLoggingService.java
@@ -115,7 +115,7 @@ public class LogbackLoggingService extends AbstractReloadableService<Object>
             throw new ComponentInitializationException("Logging configuration must be specified.");
         }
     
-        fallbackConfiguration = new ClassPathResource("/logback.xml");
+        fallbackConfiguration = new ClassPathResource("/net/shibboleth/idp/conf/logback.xml");
         loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
         statusManager = loggerContext.getStatusManager();
         if (!fallbackConfiguration.exists()) {
@@ -126,7 +126,6 @@ public class LogbackLoggingService extends AbstractReloadableService<Object>
                     + fallbackConfiguration.getDescription(), this));
         }
         super.doInitialize();
-    
     }
 
     /** {@inheritDoc} */
