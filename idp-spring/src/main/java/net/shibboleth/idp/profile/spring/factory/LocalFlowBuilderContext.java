0656144fb0d218076df9261dc026d1727e089459
Scott Cantor <cantor.2@osu.edu>
2022-07-14 13:15:13 -0400

    Revert "IDP-1975 - Reduce SWF customization/duplication"
    This reverts commit b2055032e933ba1fa3b0424ec55a55574ac8fc76.

---
+++ idp-spring/src/main/java/net/shibboleth/idp/profile/spring/factory/LocalFlowBuilderContext.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.shibboleth.idp.profile.spring.factory;
+
+import org.springframework.binding.convert.ConversionService;
+import org.springframework.binding.expression.ExpressionParser;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.validation.Validator;
+import org.springframework.webflow.core.collection.AttributeMap;
+import org.springframework.webflow.definition.registry.FlowDefinitionLocator;
+import org.springframework.webflow.engine.builder.FlowArtifactFactory;
+import org.springframework.webflow.engine.builder.FlowBuilderContext;
+import org.springframework.webflow.engine.builder.ViewFactoryCreator;
+import org.springframework.webflow.validation.ValidationHintResolver;
+
+/**
+ * This code is copied verbatim from org.springframework.webflow.engine.builder.model.LocalFlowBuilderContext
+ * 
+ * A builder context that delegates to a flow-local bean factory for builder services. Such builder services override
+ * the services of the external "parent" context.
+ * 
+ * @author Keith Donald
+ */
+class LocalFlowBuilderContext implements FlowBuilderContext {
+
+	private FlowBuilderContext parent;
+
+	private ApplicationContext localFlowContext;
+
+	public LocalFlowBuilderContext(FlowBuilderContext parent, GenericApplicationContext localFlowContext) {
+		this.parent = parent;
+		this.localFlowContext = localFlowContext;
+	}
+
+	public ApplicationContext getApplicationContext() {
+		return localFlowContext;
+	}
+
+	public String getFlowId() {
+		return parent.getFlowId();
+	}
+
+	public AttributeMap<Object> getFlowAttributes() {
+		return parent.getFlowAttributes();
+	}
+
+	public FlowDefinitionLocator getFlowDefinitionLocator() {
+		if (localFlowContext.containsLocalBean("flowRegistry")) {
+			return localFlowContext.getBean("flowRegistry", FlowDefinitionLocator.class);
+		} else {
+			return parent.getFlowDefinitionLocator();
+		}
+	}
+
+	public FlowArtifactFactory getFlowArtifactFactory() {
+		if (localFlowContext.containsLocalBean("flowArtifactFactory")) {
+			return localFlowContext.getBean("flowArtifactFactory", FlowArtifactFactory.class);
+		} else {
+			return parent.getFlowArtifactFactory();
+		}
+	}
+
+	public ConversionService getConversionService() {
+		if (localFlowContext.containsLocalBean("conversionService")) {
+			return localFlowContext.getBean("conversionService", ConversionService.class);
+		} else {
+			return parent.getConversionService();
+		}
+	}
+
+	public ViewFactoryCreator getViewFactoryCreator() {
+		if (localFlowContext.containsLocalBean("viewFactoryCreator")) {
+			return localFlowContext.getBean("viewFactoryCreator", ViewFactoryCreator.class);
+		} else {
+			return parent.getViewFactoryCreator();
+		}
+	}
+
+	public ExpressionParser getExpressionParser() {
+		if (localFlowContext.containsLocalBean("expressionParser")) {
+			return localFlowContext.getBean("expressionParser", ExpressionParser.class);
+		} else {
+			return parent.getExpressionParser();
+		}
+	}
+
+	public Validator getValidator() {
+		if (localFlowContext.containsLocalBean("validator")) {
+			return localFlowContext.getBean("validator", Validator.class);
+		} else {
+			return parent.getValidator();
+		}
+	}
+
+	public ValidationHintResolver getValidationHintResolver() {
+		if (localFlowContext.containsLocalBean("validationHintResolver")) {
+			return localFlowContext.getBean("validationHintResolver", ValidationHintResolver.class);
+		} else {
+			return parent.getValidationHintResolver();
+		}
+	}
+
+}
