3d0d8b628f15b6b1b24a80fc40433cfbec24cdc7
Scott Cantor <cantor.2@osu.edu>
2022-10-03 10:25:10 -0400

    IDP-2015 - Review web.xml for improvements via code or annotations
    https://shibboleth.atlassian.net/browse/IDP-2015
    
    Stub original MDC filter and replace with Spring-wired version.

--- idp-admin-api/src/main/java/net/shibboleth/idp/log/SLF4JMDCServletFilter.java
+++ idp-admin-api/src/main/java/net/shibboleth/idp/log/SLF4JMDCServletFilter.java
@@ -17,82 +17,15 @@
 
 package net.shibboleth.idp.log;
 
-import java.io.IOException;
-
-import javax.annotation.Nonnull;
-
-import org.slf4j.MDC;
-
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import net.shibboleth.idp.Version;
-import net.shibboleth.shared.annotation.constraint.NotEmpty;
-import net.shibboleth.shared.servlet.HttpServletSupport;
+import net.shibboleth.shared.servlet.impl.StubbedFilter;
 
 
 /**
- * Servlet filter that sets some interesting MDC attributes as the request comes in and clears the MDC as the response
- * is returned.
+ * Legacy stub.
+ * 
+ * @deprecated
  */
-public class SLF4JMDCServletFilter implements Filter {
-    
-    /** MDC attribute name for host name of the server to which the current request was sent. */
-    @Nonnull @NotEmpty public static final String SERVER_ADDRESS_MDC_ATTRIBUTE = "idp.server_hostname";
+@Deprecated(since="5.0.0", forRemoval=true)
+public class SLF4JMDCServletFilter extends StubbedFilter {
 
-    /** MDC attribute name for port number to which the current request was sent. */
-    @Nonnull @NotEmpty public static final String SERVER_PORT_MDC_ATTRIBUTE = "idp.server_port";
-    
-    /** MDC attribute name for client address. */
-    @Nonnull @NotEmpty public static final String CLIENT_ADDRESS_MDC_ATTRIBUTE = "idp.remote_addr";
-
-    /** MDC attribute name for container session ID. */
-    @Nonnull @NotEmpty public static final String JSESSIONID_MDC_ATTRIBUTE = "idp.jsessionid";
-
-    /** Whether to create a session if it doesn't already exist. */
-    private boolean createSession;
-    
-    /** {@inheritDoc} */
-    @Override
-    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
-            throws IOException, ServletException {
-        try {
-            MDC.put(Version.MDC_ATTRIBUTE, Version.getVersion());
-            MDC.put(CLIENT_ADDRESS_MDC_ATTRIBUTE, HttpServletSupport.getRemoteAddr(request));
-            MDC.put(SERVER_ADDRESS_MDC_ATTRIBUTE, request.getServerName());
-            MDC.put(SERVER_PORT_MDC_ATTRIBUTE, Integer.toString(request.getServerPort()));
-            if (request instanceof HttpServletRequest) {
-                final HttpSession session = ((HttpServletRequest) request).getSession(createSession);
-                if (session != null) {
-                    MDC.put(JSESSIONID_MDC_ATTRIBUTE, session.getId());
-                }
-            }
-            
-            chain.doFilter(request, response);
-        } finally {
-            MDC.clear();
-        }
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void init(final FilterConfig filterConfig) throws ServletException {
-        final String param = filterConfig.getInitParameter("createSession");
-        if (param != null) {
-            createSession = Boolean.valueOf(param);
-        } else {
-            createSession = true;
-        }
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void destroy() {
-        // nothing to do
-    }
 }
