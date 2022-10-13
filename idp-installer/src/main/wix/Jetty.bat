e979ef0503db47582a28070d10952edcac071534
Rod Widdowson <rdw@steadingsoftware.com>
2022-10-13 15:37:15 +0100

    IDP-1927 Make Jetty run under its own credentials for windows installs
    https://shibboleth.atlassian.net/browse/IDP-1927
    
    Initial attempt at the mechanics of adding the ACLS to the objects and the service

--- idp-installer/src/main/wix/Jetty.bat
+++ idp-installer/src/main/wix/Jetty.bat
@@ -134,12 +134,12 @@ del *.wixobj *.wixpdb
 
 REM compile Jetty and procrun contents as well as the merge module for x64
 
-"%WIX%/BIN/CANDLE" -nologo -dJettySrc=jetty-extract\%Jex% -dJettyClass=%JETTY_CLASS% -dProcrunSrc=procrun-extract -dPlatform=x86 -arch x86 jetty_contents.wxs MergeModule.wxs procrun.wxs -ext WixFirewallExtension
+"%WIX%/BIN/CANDLE" -nologo -dJettySrc=jetty-extract\%Jex% -dJettyClass=%JETTY_CLASS% -dProcrunSrc=procrun-extract -dPlatform=x86 -arch x86 jetty_contents.wxs MergeModule.wxs procrun.wxs -ext WixFirewallExtension -ext WixUtilExtension
 if ERRORLEVEL 1 goto done
 
 REM link for x64
 
-"%WIX%/BIN/LIGHT" -nologo -out Jetty-x64.msm jetty_contents.wixobj procrun.wixobj mergemodule.wixobj -ext WixFirewallExtension -sw1072
+"%WIX%/BIN/LIGHT" -nologo -out Jetty-x64.msm jetty_contents.wixobj procrun.wixobj mergemodule.wixobj -ext WixFirewallExtension -sw1072 -ext WixUtilExtension
 if ERRORLEVEL 1 goto done
 
 dir Jetty-*.msm
